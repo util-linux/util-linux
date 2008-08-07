7bd68fc5ab15848eda6c077a4404404a0eca676e
Denis ChengRq <crquan@gmail.com>
2008-08-07 17:07:49 +0800

    sfdisk: print version should end with a newline
    Signed-off-by: Denis ChengRq <crquan@gmail.com>

--- fdisk/sfdisk.c
+++ fdisk/sfdisk.c
@@ -2283,7 +2283,7 @@ read_input(char *dev, int interactive, struct disk_desc *z) {
  */
 
 static void version(void) {
-    printf("sfdisk (%s)", PACKAGE_STRING);
+    printf("sfdisk (%s)\n", PACKAGE_STRING);
 }
 
 static void
