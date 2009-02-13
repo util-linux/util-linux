b12991dd5c4f8294096723158b6d387fc3d27342
Karel Zak <kzak@redhat.com>
2009-02-13 10:54:24 +0100

    build-sys: libtoolize by libtool-2
    We need an infrastructure for stared libraries. The latest libtool-2
    seems useful and it's definitely better than the old 1.5 crap.
    
    You need to install libtool-2 when you want to run ./autogen.sh script
    after checkout from git repository.
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -3,11 +3,11 @@
 #
 # Helps generate autoconf/automake stuff, when code is checked out from SCM.
 #
-# Copyright (C) 2006 - Karel Zak <kzak@redhat.com> 
+# Copyright (C) 2006-2009 - Karel Zak <kzak@redhat.com>
 #
 
 srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=. 
+test -z "$srcdir" && srcdir=.
 
 THEDIR=`pwd`
 cd $srcdir
@@ -28,6 +28,15 @@ DIE=0
 	echo "or see http://www.gnu.org/software/autoconf"
 	DIE=1
 }
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool-2 installed to generate util-linux-ng build system."
+	echo "Download the appropriate package for your distribution,"
+	echo "or see http://www.gnu.org/software/libtool"
+	DIE=1
+}
+
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have automake installed to generate util-linux-ng build system."
@@ -53,6 +62,12 @@ test -f mount/mount.c || {
 	exit 1
 }
 
+ltver=$(libtoolize --version | awk '/^libtoolize/ { print $4 }')
+test ${ltver##2.} == "$ltver" && {
+	echo "You must have libtool version >= 2.x.x, but you have $ltver."
+	exit 1
+}
+
 echo
 echo "Generate build-system by:"
 echo "   autopoint:  $(autopoint --version | head -1)"
@@ -60,10 +75,12 @@ echo "   aclocal:    $(aclocal --version | head -1)"
 echo "   autoconf:   $(autoconf --version | head -1)"
 echo "   autoheader: $(autoheader --version | head -1)"
 echo "   automake:   $(automake --version | head -1)"
+echo "   libtoolize: $(libtoolize --version | head -1)"
 echo
 
 set -e
 autopoint --force $AP_OPTS
+libtoolize --force --copy $LT_OPTS
 aclocal -I m4 $AL_OPTS
 autoconf $AC_OPTS
 autoheader $AH_OPTS
