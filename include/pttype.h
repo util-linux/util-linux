e85a2a9392bde75b45bc9973782d8ff5bfa2b54b
Karel Zak <kzak@redhat.com>
2009-03-12 10:15:50 +0100

    lib: add pttype.c for PT types detection
    This is a small fragment from libdisk (from xfsprogs). The final
    solution will be to move the library to util-linux-ng.
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

---
+++ include/pttype.h
@@ -0,0 +1,10 @@
+#ifndef PTTYPE_H
+#define PTTYPE_H
+
+/*
+ * Note that this is a temporary solution. The final solution will be to move
+ * libdisk from xfsprogs to util-linux-ng.
+ */
+extern const char *get_pt_type(const char *device);
+
+#endif
