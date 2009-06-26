ea1498bf601a9046f29a3ce276585202b23ba278
Karel Zak <kzak@redhat.com>
2009-06-26 09:49:40 +0200

    umount: clean up help output
    Addresses-Debian-Bug: #320500
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- mount/umount.c
+++ mount/umount.c
@@ -393,9 +393,9 @@ static struct option longopts[] =
 static void
 usage (FILE *fp, int n)
 {
-  fprintf (fp, _("Usage: umount [-hV]\n"
-	       "       umount -a [-f] [-r] [-n] [-v] [-t vfstypes] [-O opts]\n"
-	       "       umount [-f] [-r] [-n] [-v] special | node...\n"));
+  fprintf (fp, _("Usage: umount -h | -V\n"
+	       "       umount -a [-d] [-f] [-r] [-n] [-v] [-t vfstypes] [-O opts]\n"
+	       "       umount [-d] [-f] [-r] [-n] [-v] special | node...\n"));
   exit (n);
 }
 
