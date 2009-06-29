a0fca6a75228794279e54980d646b5bb4a75eac1
Karel Zak <kzak@redhat.com>
2009-06-29 15:59:48 +0200

    uuidd: move uuidd files from /var/lib/libuuid to /var/run/uuidd
    Reported-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- shlibs/uuid/src/uuidd.h
+++ shlibs/uuid/src/uuidd.h
@@ -35,8 +35,8 @@
 #ifndef _UUID_UUIDD_H
 #define _UUID_UUIDD_H
 
-#define UUIDD_SOCKET_PATH	"/var/lib/libuuid/request"
-#define UUIDD_PIDFILE_PATH	"/var/lib/libuuid/uuidd.pid"
+#define UUIDD_SOCKET_PATH	"/var/run/uuidd/request"
+#define UUIDD_PIDFILE_PATH	"/var/run/uuidd/uuidd.pid"
 #define UUIDD_PATH		"/usr/sbin/uuidd"
 
 #define UUIDD_OP_GETPID			0
