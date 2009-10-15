45bda5c8dda9273d7ce0479a1931bfd9766717f7
Karel Zak <kzak@redhat.com>
2009-10-16 01:45:42 +0200

    include: use c.h in canonicalize.h
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/canonicalize.h
+++ include/canonicalize.h
@@ -1,10 +1,7 @@
 #ifndef CANONICALIZE_H
 #define CANONICALIZE_H
 
-#include <limits.h>
-#ifndef PATH_MAX
-# define PATH_MAX 4096
-#endif
+#include "c.h"	/* for PATH_MAX */
 
 extern char *canonicalize_path(const char *path);
 
