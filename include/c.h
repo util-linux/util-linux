a3e5ea3047b85b73142df4bd8c76ff14ca61b459
Karel Zak <kzak@redhat.com>
2010-02-12 21:42:46 +0100

    include: add min/max macros
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/c.h
+++ include/c.h
@@ -46,4 +46,17 @@
 # define FALSE 0
 #endif
 
+
+#define min(x, y) ({				\
+	typeof(x) _min1 = (x);			\
+	typeof(y) _min2 = (y);			\
+	(void) (&_min1 == &_min2);		\
+	_min1 < _min2 ? _min1 : _min2; })
+
+#define max(x, y) ({				\
+	typeof(x) _max1 = (x);			\
+	typeof(y) _max2 = (y);			\
+	(void) (&_max1 == &_max2);		\
+	_max1 > _max2 ? _max1 : _max2; })
+
 #endif /* UTIL_LINUX_C_H */
