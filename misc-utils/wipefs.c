bfbe29339da6ecc0257a23244d0771c6aa91b965
Karel Zak <kzak@redhat.com>
2010-02-23 14:13:28 +0100

    wipefs: fix license (GPL -> GPLv2+)
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- misc-utils/wipefs.c
+++ misc-utils/wipefs.c
@@ -4,9 +4,10 @@
  * Copyright (C) 2009 Red Hat, Inc. All rights reserved.
  * Written by Karel Zak <kzak@redhat.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it would be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
