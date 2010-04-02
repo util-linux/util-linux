4dddc2d4aa37367432f719b7c84cb8084bd7a109
Jason Borden <jborden@bluehost.com>
2010-04-02 15:52:45 +0200

    swaplabel: new command
    Print or change the label / UUID of a swap area.
    
    [kzak@redhat.com: - code refactoring
                      - add long options
                      - clean up Makefile.am]
    
    Signed-off-by: Jason Borden <jborden@bluehost.com>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/swapheader.h
+++ include/swapheader.h
@@ -10,13 +10,17 @@ struct swap_header_v1 {
 	unsigned int badpages[1];
 };
 
+
+#define SWAP_UUID_LENGTH 16
+#define SWAP_LABEL_LENGTH 16
+
 struct swap_header_v1_2 {
 	char	      bootbits[1024];    /* Space for disklabel etc. */
 	unsigned int  version;
 	unsigned int  last_page;
 	unsigned int  nr_badpages;
-	unsigned char uuid[16];
-	char	      volume_name[16];
+	unsigned char uuid[SWAP_UUID_LENGTH];
+	char	      volume_name[SWAP_LABEL_LENGTH];
 	unsigned int  padding[117];
 	unsigned int  badpages[1];
 };
