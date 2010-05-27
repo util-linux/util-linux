de878776623b120fc1e96568f4cd69c349ec2677
Karel Zak <kzak@redhat.com>
2010-05-27 16:29:56 +0200

    taskset: add NLS support, use err.h, cleanup
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- schedutils/taskset.c
+++ schedutils/taskset.c
@@ -1,14 +1,6 @@
 /*
- * taskset.c - taskset
- * Command-line utility for setting and retrieving a task's CPU affinity
- *
- * Robert Love <rml@tech9.net>		25 April 2002
- *
- * Linux kernels as of 2.5.8 provide the needed syscalls for
- * working with a task's cpu affinity.  Currently 2.4 does not
- * support these syscalls, but patches are available at:
- *
- * 	http://www.kernel.org/pub/linux/kernel/people/rml/cpu-affinity/
+ * taskset.c - command-line utility for setting and retrieving
+ *             a task's CPU affinity
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, v2, as
@@ -24,6 +16,7 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * Copyright (C) 2004 Robert Love
+ * Copyright (C) 2010 Karel Zak <kzak@redhat.com>
  */
 
 #include <stdio.h>
@@ -35,43 +28,48 @@
 #include <string.h>
 #include <ctype.h>
 #include <sys/syscall.h>
+#include <err.h>
 
 #include "cpuset.h"
+#include "nls.h"
 
-static void show_usage(const char *cmd)
+static void __attribute__((__noreturn__)) usage(FILE *out)
 {
-	fprintf(stderr, "taskset (%s)\n", PACKAGE_STRING);
-	fprintf(stderr, "usage: %s [options] [mask | cpu-list] [pid |"\
-		" cmd [args...]]\n", cmd);
-	fprintf(stderr, "set or get the affinity of a process\n\n");
-	fprintf(stderr, "  -p, --pid                  "
-		"operate on existing given pid\n");
-        fprintf(stderr, "  -c, --cpu-list             "\
-		"display and specify cpus in list format\n");
-	fprintf(stderr, "  -h, --help                 display this help\n");
-	fprintf(stderr, "  -V, --version              "\
-		"output version information\n\n");
-	fprintf(stderr, "The default behavior is to run a new command:\n");
-	fprintf(stderr, "  %s 03 sshd -b 1024\n", cmd);
-	fprintf(stderr, "You can retrieve the mask of an existing task:\n");
-	fprintf(stderr, "  %s -p 700\n", cmd);
-	fprintf(stderr, "Or set it:\n");
-	fprintf(stderr, "  %s -p 03 700\n", cmd);
-	fprintf(stderr, "List format uses a comma-separated list instead"\
-			" of a mask:\n");
-	fprintf(stderr, "  %s -pc 0,3,7-11 700\n", cmd);
-	fprintf(stderr, "Ranges in list format can take a stride argument:\n");
-	fprintf(stderr, "  e.g. 0-31:2 is equivalent to mask 0x55555555\n\n");
+	fprintf(out,
+		_("Usage: %s [options] [mask | cpu-list] [pid|cmd [args...]]\n\n"),
+		program_invocation_short_name);
+
+	fprintf(out, _(
+		"Options:\n"
+		" -p, --pid               operate on existing given pid\n"
+		" -c, --cpu-list          display and specify cpus in list format\n"
+		" -h, --help              display this help\n"
+		" -V, --version           output version information\n\n"));
+
+	fprintf(out, _(
+		"The default behavior is to run a new command:\n"
+		"    %1$s 03 sshd -b 1024\n"
+		"You can retrieve the mask of an existing task:\n"
+		"    %1$s -p 700\n"
+		"Or set it:\n"
+		"    %1$s -p 03 700\n"
+		"List format uses a comma-separated list instead of a mask:\n"
+		"    %1$s -pc 0,3,7-11 700\n"
+		"Ranges in list format can take a stride argument:\n"
+		"    e.g. 0-31:2 is equivalent to mask 0x55555555\n"),
+		program_invocation_short_name);
+
+	fprintf(out, _("\nFor more information see taskset(1).\n"));
+
+	exit(out == stderr ? EXIT_FAILURE : EXIT_SUCCESS);
 }
 
 /*
  * Number of bits in a CPU bitmask on current system
  */
-static int
-max_number_of_cpus(void)
+static int max_number_of_cpus(void)
 {
-	int n;
-	int cpus = 2048;
+	int n, cpus = 2048;
 	size_t setsize;
 	cpu_set_t *set = cpuset_alloc(cpus, &setsize, NULL);
 
@@ -84,7 +82,7 @@ max_number_of_cpus(void)
 		/* the library version does not return size of cpumask_t */
 		n = syscall(SYS_sched_getaffinity, 0, setsize, set);
 
-		if (n < 0 && errno == EINVAL && cpus < 1024*1024) {
+		if (n < 0 && errno == EINVAL && cpus < 1024 * 1024) {
 			cpuset_free(set);
 			cpus *= 2;
 			set = cpuset_alloc(cpus, &setsize, NULL);
@@ -93,12 +91,10 @@ max_number_of_cpus(void)
 			continue;
 		}
 		cpuset_free(set);
-		return n*8;
+		return n * 8;
 	}
-
 err:
-	fprintf (stderr, "cannot determine NR_CPUS; aborting");
-	exit(1);
+	errx(EXIT_FAILURE, _("cannot determine NR_CPUS; aborting"));
 	return 0;
 }
 
@@ -106,9 +102,8 @@ int main(int argc, char *argv[])
 {
 	cpu_set_t *new_set, *cur_set;
 	pid_t pid = 0;
-	int opt, err;
+	int opt, c_opt = 0, rc;
 	char *buf;
-	int c_opt = 0;
         unsigned int ncpus;
 	size_t new_setsize, cur_setsize, cur_nbits, buflen;
 
@@ -120,9 +115,11 @@ int main(int argc, char *argv[])
 		{ NULL,		0, NULL, 0 }
 	};
 
-	while ((opt = getopt_long(argc, argv, "+pchV", longopts, NULL)) != -1) {
-		int ret = 1;
+	setlocale(LC_ALL, "");
+	bindtextdomain(PACKAGE, LOCALEDIR);
+	textdomain(PACKAGE);
 
+	while ((opt = getopt_long(argc, argv, "+pchV", longopts, NULL)) != -1) {
 		switch (opt) {
 		case 'p':
 			pid = atoi(argv[argc - 1]);
@@ -132,98 +129,80 @@ int main(int argc, char *argv[])
 			break;
 		case 'V':
 			printf("taskset (%s)\n", PACKAGE_STRING);
-			return 0;
+			return EXIT_SUCCESS;
 		case 'h':
-			ret = 0;
+			usage(stdout);
+			break;
 		default:
-			show_usage(argv[0]);
-			return ret;
+			usage(stderr);
+			break;
 		}
 	}
 
 	if ((!pid && argc - optind < 2)
-			|| (pid && (argc - optind < 1 || argc - optind > 2))) {
-		show_usage(argv[0]);
-		return 1;
-	}
+			|| (pid && (argc - optind < 1 || argc - optind > 2)))
+		usage(stderr);
 
 	ncpus = max_number_of_cpus();
 
 	/*
-	 * cur_mask is always used for the sched_getaffinity call
+	 * cur_set is always used for the sched_getaffinity call
 	 * On the sched_getaffinity the kernel demands a user mask of
 	 * at least the size of its own cpumask_t.
 	 */
 	cur_set = cpuset_alloc(ncpus, &cur_setsize, &cur_nbits);
-	if (!cur_set) {
-		fprintf (stderr, "cpuset_alloc failed\n");
-		exit(1);
-	}
+	if (!cur_set)
+		err(EXIT_FAILURE, _("cpuset_alloc failed"));
+
 	buflen = 7 * cur_nbits;
 	buf = malloc(buflen);
+	if (!buf)
+		err(EXIT_FAILURE, _("malloc failed"));
 
 	/*
-	 * new_mask is always used for the sched_setaffinity call
+	 * new_set is always used for the sched_setaffinity call
 	 * On the sched_setaffinity the kernel will zero-fill its
 	 * cpumask_t if the user's mask is shorter.
 	 */
 	new_set = cpuset_alloc(ncpus, &new_setsize, NULL);
-	if (!new_set) {
-		fprintf (stderr, "cpuset_alloc failed\n");
-		exit(1);
-	}
+	if (!new_set)
+		err(EXIT_FAILURE, _("cpuset_alloc failed"));
 
 	if (pid) {
-		if (sched_getaffinity(pid, cur_setsize, cur_set) < 0) {
-			perror("sched_getaffinity");
-			fprintf(stderr, "failed to get pid %d's affinity\n",
-				pid);
-			return 1;
-		}
+		if (sched_getaffinity(pid, cur_setsize, cur_set) < 0)
+			err(EXIT_FAILURE, _("failed to get pid %d's affinity"), pid);
+
 		if (c_opt)
-			printf("pid %d's current affinity list: %s\n", pid,
+			printf(_("pid %d's current affinity list: %s\n"), pid,
 				cpulist_create(buf, buflen, cur_set, cur_setsize));
 		else
-			printf("pid %d's current affinity mask: %s\n", pid,
+			printf(_("pid %d's current affinity mask: %s\n"), pid,
 				cpumask_create(buf, buflen, cur_set, cur_setsize));
 
 		if (argc - optind == 1)
-			return 0;
+			return EXIT_SUCCESS;
 	}
 
-	if (c_opt)
-		err = cpulist_parse(argv[optind], new_set, new_setsize);
-	else
-		err = cpumask_parse(argv[optind], new_set, new_setsize);
+	rc = c_opt ? cpulist_parse(argv[optind], new_set, new_setsize) :
+		     cpumask_parse(argv[optind], new_set, new_setsize);
 
-	if (err) {
-		if (c_opt)
-			fprintf(stderr, "failed to parse CPU list %s\n",
+	if (rc)
+		errx(EXIT_FAILURE, _("failed to parse %s %s"),
+				c_opt ? _("CPU list") : _("CPU mask"),
 				argv[optind]);
-		else
-			fprintf(stderr, "failed to parse CPU mask %s\n",
-				argv[optind]);
-		return 1;
-	}
 
-	if (sched_setaffinity(pid, new_setsize, new_set) < 0) {
-		perror("sched_setaffinity");
-		fprintf(stderr, "failed to set pid %d's affinity.\n", pid);
-		return 1;
-	}
+	if (sched_setaffinity(pid, new_setsize, new_set) < 0)
+		err(EXIT_FAILURE, _("failed to set pid %d's affinity"), pid);
 
-	if (sched_getaffinity(pid, cur_setsize, cur_set) < 0) {
-		perror("sched_getaffinity");
-		fprintf(stderr, "failed to get pid %d's affinity.\n", pid);
-		return 1;
-	}
+	if (sched_getaffinity(pid, cur_setsize, cur_set) < 0)
+		err(EXIT_FAILURE, _("failed to get pid %d's affinity"), pid);
 
 	if (pid) {
 		if (c_opt)
-			printf("pid %d's new affinity list: %s\n", pid,
+			printf(_("pid %d's new affinity list: %s\n"), pid,
 				cpulist_create(buf, buflen, cur_set, cur_setsize));
 		else
-			printf("pid %d's new affinity mask: %s\n", pid,
+			printf(_("pid %d's new affinity mask: %s\n"), pid,
 				cpumask_create(buf, buflen, cur_set, cur_setsize));
 	}
 
@@ -234,10 +213,8 @@ int main(int argc, char *argv[])
 	if (!pid) {
 		argv += optind + 1;
 		execvp(argv[0], argv);
-		perror("execvp");
-		fprintf(stderr, "failed to execute %s\n", argv[0]);
-		return 1;
+		err(EXIT_FAILURE, _("executing %s failed"), argv[0]);
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
