1cf4c20b198c0c6566198fd00b983d9aaf8321bc
Miklos Szeredi <miklos@szeredi.hu>
2010-10-07 16:05:12 +0200

    mount: don't canonicalize "spec" with --no-canonicalize option
    "Spec" was still canonicalized despite --no-canonicalize.  This
    resulted in a hang during login with pam_encfs (Debian Bug#593336).
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>

--- mount/devname.c
+++ mount/devname.c
@@ -8,7 +8,7 @@ spec_to_devname(const char *spec)
 {
 	if (!spec)
 		return NULL;
-	if (is_pseudo_fs(spec))
+	if (nocanonicalize || is_pseudo_fs(spec))
 		return xstrdup(spec);
 	return fsprobe_get_devname_by_spec(spec);
 }
