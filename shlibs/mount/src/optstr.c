e548d51d552f2818064e218809a7ac00e89b6f39
Karel Zak <kzak@redhat.com>
2010-11-19 13:15:19 +0100

    libmount: fix mnt_optstr_remove_option_at()
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- shlibs/mount/src/optstr.c
+++ shlibs/mount/src/optstr.c
@@ -297,13 +297,15 @@ int mnt_optstr_get_option(char *optstr, const char *name,
  */
 int mnt_optstr_remove_option_at(char **optstr, char *begin, char *end)
 {
-	size_t sz = strlen(end);
+	size_t sz;
 
 	if (!optstr || !begin || !end)
 		return -EINVAL;
 
 	if ((begin == *optstr || *(begin - 1) == ',') && *end == ',')
 		end++;
+
+	sz = strlen(end);
 
 	memmove(begin, end, sz + 1);
 	if (!*begin && *(begin - 1) == ',')
