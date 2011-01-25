9f55bf3794ca98852dd1f352f993dea60d83a2e1
Fabian Groffen <grobian@gentoo.org>
2011-01-25 22:48:15 +0100

    libblkid: conditionalise parts from blkid/topology for Linux
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- shlibs/blkid/src/topology/topology.h
+++ shlibs/blkid/src/topology/topology.h
@@ -11,12 +11,14 @@ extern int blkid_topology_set_physical_sector_size(blkid_probe pr, unsigned long
 /*
  * topology probers
  */
+#ifdef __linux__
 extern const struct blkid_idinfo ioctl_tp_idinfo;
-extern const struct blkid_idinfo sysfs_tp_idinfo;
 extern const struct blkid_idinfo md_tp_idinfo;
-extern const struct blkid_idinfo dm_tp_idinfo;
 extern const struct blkid_idinfo evms_tp_idinfo;
+extern const struct blkid_idinfo sysfs_tp_idinfo;
+extern const struct blkid_idinfo dm_tp_idinfo;
 extern const struct blkid_idinfo lvm_tp_idinfo;
+#endif
 
 #endif /* BLKID_TOPOLOGY_H */
 
