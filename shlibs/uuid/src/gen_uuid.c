d6c8d399b93f4646a90d0180fdcc1be35235a9c7
Petr Uzel <petr.uzel@suse.cz>
2011-02-09 13:14:04 +0100

    libuuid: do not check for EAGAIN after flock()
    As per flock() manpage, flock() does not set errno=EAGAIN.
    
    Signed-off-by: Petr Uzel <petr.uzel@suse.cz>

--- shlibs/uuid/src/gen_uuid.c
+++ shlibs/uuid/src/gen_uuid.c
@@ -333,7 +333,7 @@ static int get_clock(uint32_t *clock_high, uint32_t *clock_low,
 	if (state_fd >= 0) {
 		rewind(state_f);
 		while (flock(state_fd, LOCK_EX) < 0) {
-			if ((errno == EAGAIN) || (errno == EINTR))
+			if (errno == EINTR)
 				continue;
 			fclose(state_f);
 			close(state_fd);
