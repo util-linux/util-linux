289127f52ddaeec8484d5321a3169a2bb5026b58
Heiko Carstens <heiko.carstens@de.ibm.com>
2011-09-09 11:19:31 +0200

    lib,cpuset: fix odd placed braces in cpulist_parse()
    The opening and closing braces for two following if statements within
    cpulist_parse() are placed in an odd manner.
    Just fix this to prevent broken code in the future.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- lib/cpuset.c
+++ lib/cpuset.c
@@ -289,8 +289,8 @@ int cpulist_parse(const char *str, cpu_set_t *set, size_t setsize, int fail)
 			if (sscanf(c1, "%u", &b) < 1)
 				return 1;
 			c1 = nexttoken(c1, ':');
-			if (c1 != NULL && (c2 == NULL || c1 < c2))
-				if (sscanf(c1, "%u", &s) < 1) {
+			if (c1 != NULL && (c2 == NULL || c1 < c2)) {
+				if (sscanf(c1, "%u", &s) < 1)
 					return 1;
 			}
 		}
