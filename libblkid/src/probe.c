a3ab71cfd3f2b2240e05df3e9cc0800ab4578744
Karel Zak <kzak@redhat.com>
2011-11-10 20:18:54 +0100

    libblkid: don't call CDROM_GET_CAPABILITY for partitions
    Reported-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- libblkid/src/probe.c
+++ libblkid/src/probe.c
@@ -707,7 +707,10 @@ int blkid_probe_set_device(blkid_probe pr, int fd,
 		pr->flags |= BLKID_FL_TINY_DEV;
 
 #ifdef CDROM_GET_CAPABILITY
-	if (S_ISBLK(sb.st_mode) && ioctl(fd, CDROM_GET_CAPABILITY, NULL) >= 0)
+	if (S_ISBLK(sb.st_mode) &&
+	    !blkid_probe_is_tiny(pr) &&
+	    blkid_probe_is_wholedisk(pr) &&
+	    ioctl(fd, CDROM_GET_CAPABILITY, NULL) >= 0)
 		pr->flags |= BLKID_FL_CDROM_DEV;
 #endif
 
