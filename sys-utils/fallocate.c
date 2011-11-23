f75b8e5cdfa40b81a0b631b02491a99115409371
Karel Zak <kzak@redhat.com>
2011-11-23 15:19:20 +0100

    fallocate: clean up fallbacks for FALLOC_FL_* flags
    Reported-by: Voelker, Bernhard <bernhard.voelker@siemens-enterprise.com>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- sys-utils/fallocate.c
+++ sys-utils/fallocate.c
@@ -38,8 +38,13 @@
 
 #ifdef HAVE_LINUX_FALLOC_H
 # include <linux/falloc.h>	/* for FALLOC_FL_* flags */
-#else
+#endif
+
+#ifndef FALLOC_FL_KEEP_SIZE
 # define FALLOC_FL_KEEP_SIZE 1
+#endif
+
+#ifndef FALLOC_FL_PUNCH_HOLE
 # define FALLOC_FL_PUNCH_HOLE 2
 #endif
 
