607e6b7ce8658e552fdffbb99c51cdb01828e27b
Karel Zak <kzak@redhat.com>
2012-02-28 11:16:16 +0100

    login: support crazy shadow-utils syntax in login.defs
    standard syntax for FOO=data:
    
    	ENV_FOO   data
    
    additional syntax:
    
    	ENV_FOO  FOO=data
    
    Reported-by: Colin Guthrie colin@mageia.org
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- login-utils/logindefs.h
+++ login-utils/logindefs.h
@@ -5,5 +5,6 @@ extern int getlogindefs_bool(const char *name, int dflt);
 extern long getlogindefs_num(const char *name, long dflt);
 extern const char *getlogindefs_str(const char *name, const char *dflt);
 extern void free_getlogindefs_data(void);
+extern int logindefs_setenv(const char *name, const char *conf, const char *dflt);
 
 #endif /* UTIL_LINUX_LOGINDEFS_H */
