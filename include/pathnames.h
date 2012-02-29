79b128072502544e8e8ede0b46d93c052ac575a1
Davidlohr Bueso <dave@gnu.org>
2012-02-29 12:27:41 +0100

    include: add /proc/locks path to pathnames
    Signed-off-by: Davidlohr Bueso <dave@gnu.org>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/pathnames.h
+++ include/pathnames.h
@@ -88,6 +88,7 @@
 #define _PATH_PROC_PARTITIONS	"/proc/partitions"
 #define _PATH_PROC_DEVICES	"/proc/devices"
 #define _PATH_PROC_MOUNTINFO	"/proc/self/mountinfo"
+#define _PATH_PROC_LOCKS        "/proc/locks"
 
 #define _PATH_SYS_BLOCK		"/sys/block"
 #define _PATH_SYS_DEVBLOCK	"/sys/dev/block"
