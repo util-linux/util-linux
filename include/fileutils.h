6b79eb38ba46a7635a635623b91c2e3aa9326c7d
Sami Kerola <kerolasa@iki.fi>
2012-02-29 15:58:51 +0100

    lib: add fileutils function collection
    The fileutils contains xmkstemp function will create temporary file
    safe and reusable manner.
    
    Reference: http://www.dwheeler.com/secure-programs/Secure-Programs-HOWTO.html#TEMPORARY-FILES
    CC: Davidlohr Bueso <dave@gnu.org>
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

---
+++ include/fileutils.h
@@ -0,0 +1,6 @@
+#ifndef UTIL_LINUX_FILEUTILS
+#define UTIL_LINUX_FILEUTILS
+
+extern FILE * xmkstemp(char **tmpname);
+
+#endif
---
+++ lib/fileutils.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2012 Sami Kerola <kerolasa@iki.fi>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "c.h"
+#include "pathnames.h"
+
+/* Create open temporary file in safe way.  Please notice that the
+ * file permissions are -rw------- by default. */
+FILE *xmkstemp(char **tmpname)
+{
+	char *localtmp;
+	char *tmpenv;
+	mode_t old_mode;
+	int fd;
+	FILE *ret;
+
+	tmpenv = getenv("TMPDIR");
+	if (tmpenv)
+		asprintf(&localtmp, "%s/%s.XXXXXX", tmpenv,
+			 program_invocation_short_name);
+	else
+		asprintf(&localtmp, "%s/%s.XXXXXX", _PATH_TMP,
+			 program_invocation_short_name);
+	old_mode = umask(077);
+	fd = mkstemp(localtmp);
+	umask(old_mode);
+	if (fd == -1)
+		return NULL;
+	if (!(ret = fdopen(fd, "w+")))
+		goto err;
+	*tmpname = localtmp;
+	return ret;
+ err:
+	close(fd);
+	return NULL;
+}
+
+#ifdef TEST_PROGRAM
+int main(void)
+{
+	FILE *f;
+	char *tmpname;
+	f = xmkstemp(&tmpname);
+	unlink(tmpname);
+	free(tmpname);
+	fclose(f);
+	return EXIT_FAILURE;
+}
+#endif
