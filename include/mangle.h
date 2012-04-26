95387b669658402cad6bce9bb18cecd8b11af092
Karel Zak <kzak@redhat.com>
2012-04-26 09:17:44 +0200

    lib/mangle: cleanup, add unhexmangle
     * use strchr() rather than for()
     * small refactoring in mangle code
     * add un-hex-mangle
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/mangle.h
+++ include/mangle.h
@@ -8,11 +8,18 @@
 extern char *mangle(const char *s);
 
 extern void unmangle_to_buffer(const char *s, char *buf, size_t len);
+void unhexmangle_to_buffer(const char *s, char *buf, size_t len);
+
 extern char *unmangle(const char *s, char **end);
 
 static inline void unmangle_string(char *s)
 {
 	unmangle_to_buffer(s, s, strlen(s) + 1);
+}
+
+static inline void unhexmangle_string(char *s)
+{
+	unhexmangle_to_buffer(s, s, strlen(s) + 1);
 }
 
 #endif /* UTIL_LINUX_MANGLE_H */
