cd3d6c5bd0d2d4350972dc38d53753b5581c1941
Dave Reisner <dreisner@archlinux.org>
2012-04-25 20:30:51 -0400

    libmount: expose mnt_get_mountpoint as external API

--- libmount/src/mountP.h
+++ libmount/src/mountP.h
@@ -139,7 +139,6 @@ extern int mnt_get_uid(const char *username, uid_t *uid);
 extern int mnt_get_gid(const char *groupname, gid_t *gid);
 extern int mnt_in_group(gid_t gid);
 
-extern char *mnt_get_mountpoint(const char *path);
 extern char *mnt_get_fs_root(const char *path, const char *mountpoint);
 extern int mnt_open_uniq_filename(const char *filename, char **name);
 extern int mnt_has_regular_utab(const char **utab, int *writable);
