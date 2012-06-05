90af3fe39bb843a237e599748db342d4a05482e9
Karel Zak <kzak@redhat.com>
2012-06-05 15:44:12 +0200

    inclide/env: innclude c.h, remove _() macro from xsetenv()
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/env.h
+++ include/env.h
@@ -1,6 +1,8 @@
 #ifndef UTIL_LINUX_ENV_H
 #define UTIL_LINUX_ENV_H
 
+#include "c.h"
+
 extern void sanitize_env (void);
 extern char *safe_getenv(const char *arg);
 
@@ -8,7 +10,7 @@ static inline void
 xsetenv (char const *name, char const *val, int overwrite)
 {
   if (setenv (name, val, overwrite) != 0)
-    err (EXIT_FAILURE,  _("failed to set the %s environment variable"), name);
+    err (EXIT_FAILURE,  "failed to set the %s environment variable", name);
 }
 
 #endif /* UTIL_LINUX_ENV_H */
