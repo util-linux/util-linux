f5ee44b17a38293c33f39884ceb43963c29ce7ad
Sami Kerola <kerolasa@iki.fi>
2012-07-25 21:15:25 +0200

    blkid: fix shadow declaration
    misc-utils/blkid.c:402:15: warning: declaration of 'usage' shadows a global declaration [-Wshadow]
    misc-utils/blkid.c:63:13: warning: shadowed declaration is here [-Wshadow]
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- misc-utils/blkid.c
+++ misc-utils/blkid.c
@@ -399,17 +399,17 @@ static int print_udev_ambivalent(blkid_probe pr)
 	int count = 0, rc = -1;
 
 	while (!blkid_do_probe(pr)) {
-		const char *usage = NULL, *type = NULL, *version = NULL;
+		const char *usage_txt = NULL, *type = NULL, *version = NULL;
 		char enc[256];
 
-		blkid_probe_lookup_value(pr, "USAGE", &usage, NULL);
+		blkid_probe_lookup_value(pr, "USAGE", &usage_txt, NULL);
 		blkid_probe_lookup_value(pr, "TYPE", &type, NULL);
 		blkid_probe_lookup_value(pr, "VERSION", &version, NULL);
 
-		if (!usage || !type)
+		if (!usage_txt || !type)
 			continue;
 
-		blkid_encode_string(usage, enc, sizeof(enc));
+		blkid_encode_string(usage_txt, enc, sizeof(enc));
 		if (append_str(&val, &valsz, enc, ":"))
 			goto done;
 
