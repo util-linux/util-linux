e0b6ab56871b8122dca5ba6237b32687ead8232c
Sami Kerola <kerolasa@iki.fi>
2012-10-17 20:26:14 +0100

    include/env: unify indentation
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/env.h
+++ include/env.h
@@ -3,14 +3,13 @@
 
 #include "c.h"
 
-extern void sanitize_env (void);
+extern void sanitize_env(void);
 extern char *safe_getenv(const char *arg);
 
-static inline void
-xsetenv (char const *name, char const *val, int overwrite)
+static inline void xsetenv(char const *name, char const *val, int overwrite)
 {
-  if (setenv (name, val, overwrite) != 0)
-    err (EXIT_FAILURE,  "failed to set the %s environment variable", name);
+	if (setenv(name, val, overwrite) != 0)
+		err(EXIT_FAILURE, "failed to set the %s environment variable", name);
 }
 
 #endif /* UTIL_LINUX_ENV_H */
