74b89d7d7a80b0d26489fd3fa7bac831144e599c
Karel Zak <kzak@redhat.com>
2012-11-13 18:05:36 +0100

    fdisk: don't offer GPT stuff in BSD menu
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- fdisks/fdisk.c
+++ fdisks/fdisk.c
@@ -90,7 +90,7 @@ static const struct menulist_descr menulist[] = {
 	{'e', N_("edit drive data"), {OSF_LABEL, 0}},
 	{'f', N_("fix partition order"), {0, DOS_LABEL}},
 	{'g', N_("create an IRIX (SGI) partition table"), {0, ANY_LABEL}},
-	{'g', N_("create a new empty GPT partition table"), {ANY_LABEL, 0}},
+	{'g', N_("create a new empty GPT partition table"), {~OSF_LABEL, 0}},
 	{'h', N_("change number of heads"), {0, DOS_LABEL | SUN_LABEL}},
 	{'i', N_("change interleave factor"), {0, SUN_LABEL}},
 	{'i', N_("change the disk identifier"), {0, DOS_LABEL}},
