c717b0328d0dd111f97b8e19b947249833a909fe
Karel Zak <kzak@redhat.com>
2012-12-10 13:14:38 +0100

    rename: use macro to print version
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- misc-utils/rename.c
+++ misc-utils/rename.c
@@ -136,9 +136,7 @@ int main(int argc, char **argv)
 			symtarget = 1;
 			break;
 		case 'V':
-			printf(_("%s from %s\n"),
-			       program_invocation_short_name,
-			       PACKAGE_STRING);
+			printf(UTIL_LINUX_VERSION);
 			return EXIT_SUCCESS;
 		case 'h':
 			usage(stdout);
