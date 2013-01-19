29bbf5abd52e71dd3ec21d590d4f7bd47ef7fe36
Sami Kerola <kerolasa@iki.fi>
2013-01-19 00:08:55 +0000

    tests: add tailf(1) check
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- tests/commands.sh
+++ tests/commands.sh
@@ -68,3 +68,4 @@ TS_CMD_HEXDUMP=${TS_CMD_HEXDUMP-"$top_builddir/hexdump"}
 TS_CMD_UTMPDUMP=${TS_CMD_UTMPDUMP-"$top_builddir/utmpdump"}
 TS_CMD_GETOPT=${TS_CMD_GETOPT-"$top_builddir/getopt"}
 TS_CMD_MCOOKIE=${TS_CMD_MCOOKIE-"$top_builddir/mcookie"}
+TS_CMD_TAILF=${TS_CMD_TAILF-"$top_builddir/tailf"}
