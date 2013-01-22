c2f0a85671323410c5c372f642d4b1014fb11322
Sami Kerola <kerolasa@iki.fi>
2013-01-22 23:27:08 +0000

    hwclock: make usage() change translator friendly
    The --compare option from 83765871ef0d6c554c46027577648b77c7351a78 is
    made to be separate fputs() line.
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- sys-utils/hwclock.c
+++ sys-utils/hwclock.c
@@ -1443,8 +1443,8 @@ static void usage(const char *fmt, ...)
 		" -w, --systohc        set the hardware clock from the current system time\n"
 		"     --systz          set the system time based on the current timezone\n"
 		"     --adjust         adjust the RTC to account for systematic drift since\n"
-		"                        the clock was last set or adjusted\n"
-		" -c, --compare        periodically compare the system clock with the CMOS clock\n"), usageto);
+		"                        the clock was last set or adjusted\n"), usageto);
+	fputs(_(" -c, --compare        periodically compare the system clock with the CMOS clock\n"), usageto);
 #ifdef __linux__
 	fputs(_("     --getepoch       print out the kernel's hardware clock epoch value\n"
 		"     --setepoch       set the kernel's hardware clock epoch value to the \n"
