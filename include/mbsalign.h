36c7f78524f852d9d9362e9d8baaad28dd20e5ee
Pádraig Brady <P@draigBrady.com>
2013-03-15 09:48:37 +0000

    lib/mbsalign: correct license header from GPLv[23] to LGPLv2+
    * lib/mbsalign.c: s/GPLv3/LGPLv2+/
    * include/mbsalign.h: s/GPLv2/LGPLv2+/
    * README.licensing: Remove mention GPLv3 as it's not actually used.

--- include/mbsalign.h
+++ include/mbsalign.h
@@ -2,8 +2,8 @@
    Copyright (C) 2009-2010 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 2 of the License, or
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 2.1 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
