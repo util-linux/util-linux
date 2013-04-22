0bd341508ec1a002b1ae4caf722671c5c5af4be3
Benno Schulenberg <bensberg@justemail.net>
2013-04-22 10:06:36 +0200

    su: consistent angular brackets and lowercase in help text of 'su'
    Signed-off-by: Benno Schulenberg <bensberg@justemail.net>

--- login-utils/su-common.c
+++ login-utils/su-common.c
@@ -627,24 +627,23 @@ usage (int status)
 {
   if (su_mode == RUNUSER_MODE) {
     fputs(USAGE_HEADER, stdout);
-    printf (_(" %s [options] -u <USER> COMMAND\n"), program_invocation_short_name);
-    printf (_(" %s [options] [-] [USER [arg]...]\n"), program_invocation_short_name);
+    printf (_(" %s [options] -u <user> <command>\n"), program_invocation_short_name);
+    printf (_(" %s [options] [-] [<user> [<argument>...]]\n"), program_invocation_short_name);
     fputs (_("\n"
-    "Run COMMAND with the effective <user> id and group id. If -u not\n"
-    "given, fallback to su(1) compatible semantic and shell is executed.\n"
+    "Run <command> with the effective user ID and group ID of <user>.  If -u is\n"
+    "not given, fall back to su(1)-compatible semantics and execute standard shell.\n"
     "The options -l, -c, -f, -s are mutually exclusive to -u.\n"), stdout);
 
     fputs(USAGE_OPTIONS, stdout);
 
-    fputs (_(
-    " -u, --user <user>               username\n"), stdout);
+    fputs (_(" -u, --user <user>             username\n"), stdout);
 
   } else {
     fputs(USAGE_HEADER, stdout);
-    printf (_(" %s [options] [-] [USER [arg]...]\n"), program_invocation_short_name);
+    printf (_(" %s [options] [-] [<user> [<argument>...]]\n"), program_invocation_short_name);
     fputs (_("\n"
-    "Change the effective user id and group id to that of USER.\n"
-    "A mere - implies -l.   If USER not given, assume root.\n"), stdout);
+    "Change the effective user ID and group ID to that of <user>.\n"
+    "A mere - implies -l.  If <user> is not given, root is assumed.\n"), stdout);
 
     fputs(USAGE_OPTIONS, stdout);
   }
@@ -655,10 +654,10 @@ usage (int status)
 
   fputs (_(" -, -l, --login                  make the shell a login shell\n"), stdout);
   fputs (_(" -c, --command <command>         pass a single command to the shell with -c\n"), stdout);
-  fputs (_(" --session-command <command>     pass a single command to the shell with -c\n"), stdout);
-  fputs (_("                                 and do not create a new session\n"), stdout);
+  fputs (_(" --session-command <command>     pass a single command to the shell with -c\n"
+           "                                   and do not create a new session\n"), stdout);
   fputs (_(" -f, --fast                      pass -f to the shell (for csh or tcsh)\n"), stdout);
-  fputs (_(" -s, --shell <shell>             run shell if /etc/shells allows it\n"), stdout);
+  fputs (_(" -s, --shell <shell>             run <shell> if /etc/shells allows it\n"), stdout);
 
   fputs(USAGE_SEPARATOR, stdout);
   fputs(USAGE_HELP, stdout);
