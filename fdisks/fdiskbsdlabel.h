ef083b7cf5059257ef64f7f429e2dc4f12e5036f
Karel Zak <kzak@redhat.com>
2013-06-26 11:11:59 +0200

    fdisk: (bsd) clean up BSD_LABEL* macros
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- fdisks/fdiskbsdlabel.h
+++ fdisks/fdiskbsdlabel.h
@@ -41,26 +41,21 @@
 #endif
 
 #ifndef BSD_MAXPARTITIONS
-#define	BSD_MAXPARTITIONS 16
+# define BSD_MAXPARTITIONS 16
 #endif
 
 #define BSD_LINUX_BOOTDIR "/usr/ucb/mdec"
 
-#if defined (__i386__) || defined (__sparc__) || defined (__arm__) || \
-    defined (__mips__) || defined (__s390__) || defined (__sh__) || \
-    defined (__aarch64__) || defined(__xtensa__) || \
-    defined (__x86_64__) || defined (__avr32__) || defined(__cris__) || \
-    defined (__microblaze__)
-#define BSD_LABELSECTOR   1
-#define BSD_LABELOFFSET   0
-#elif defined (__alpha__) || defined (__powerpc__) || defined (__ia64__) || defined (__hppa__)
-#define BSD_LABELSECTOR   0
-#define BSD_LABELOFFSET   64
-#elif defined (__s390__) || defined (__s390x__)
-#define BSD_LABELSECTOR   1
-#define BSD_LABELOFFSET   0
+#if defined (__alpha__) || defined (__powerpc__) || \
+    defined (__ia64__) || defined (__hppa__)
+# define BSD_LABELSECTOR   0
+# define BSD_LABELOFFSET   64
+#elif defined(__sparc64__)
+# define BSD_LABELSECTOR   0
+# define BSD_LABELOFFSET   128
 #else
-#error unknown architecture
+# define BSD_LABELSECTOR   1
+# define BSD_LABELOFFSET   0
 #endif
 
 #define	BSD_BBSIZE        8192		/* size of boot area, with label */
