ee612ea91d7b7c55fb06fa96bf74165d1c1d2b95
Mike Frysinger <vapier@gentoo.org>
2013-06-27 20:05:18 -0400

    exec_shell: add a license and touch up func def
    When this file was created, the standard license header was missed.
    Add it using the same one from unshare.c.
    
    The noreturn attribute is not needed since we include the header
    which has it on the prototype.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- lib/exec_shell.c
+++ lib/exec_shell.c
@@ -1,3 +1,21 @@
+/*
+ * exec_shell() - launch a shell, else exit!
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -11,7 +29,8 @@
 
 #define DEFAULT_SHELL "/bin/sh"
 
-void __attribute__((__noreturn__)) exec_shell(void) {
+void exec_shell(void)
+{
 	const char *shell = getenv("SHELL"), *shell_basename;
 	char *arg0;
 	if (!shell)
