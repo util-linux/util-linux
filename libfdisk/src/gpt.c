d0ccfeb193b87a1dc7356cf274f64f49ccc67d1a
Karel Zak <kzak@redhat.com>
2013-07-10 17:33:09 +0200

    libfdisk: explicitly add COPYING file
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- libfdisk/src/gpt.c
+++ libfdisk/src/gpt.c
@@ -5,22 +5,7 @@
  * GUID Partition Table (GPT) support. Based on UEFI Specs 2.3.1
  * Chapter 5: GUID Partition Table (GPT) Disk Layout (Jun 27th, 2012).
  * Some ideas and inspiration from GNU parted and gptfdisk.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
