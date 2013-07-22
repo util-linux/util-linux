0dde19974bcbb07ce977992ab04b7dfa752d99b3
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2013-07-22 12:56:47 +0100

    setsid: add an option to wait child return value
    This change proposed in util-linux maillist by Damien Wyart in behalf of
    Daniel Kahn Gillmor.  Sami Kerola changed the patch to use option rather
    than makign the default behavior of the command to change.
    
    Reported-by: Damien Wyart <damien.wyart@gmail.com>
    Reference: http://markmail.org/message/2z6qh5auk7wjie3l
    Bug-Debian: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=495881
    CC: Daniel Kahn Gillmor <dkg@fifthhorseman.net>
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- sys-utils/setsid.c
+++ sys-utils/setsid.c
@@ -9,6 +9,8 @@
  * 2001-01-18 John Fremlin <vii@penguinpowered.com>
  * - fork in case we are process group leader
  *
+ * 2008-08-20 Daniel Kahn Gillmor <dkg@fifthhorseman.net>
+ * - if forked, wait on child process and emit its return code.
  */
 
 #include <getopt.h>
@@ -16,6 +18,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "c.h"
 #include "nls.h"
@@ -29,8 +33,8 @@ static void __attribute__ ((__noreturn__)) usage(FILE * out)
 		program_invocation_short_name);
 
 	fputs(USAGE_OPTIONS, out);
-	fputs(_(" -c, --ctty     set the controlling terminal to the current one\n"),
-		out);
+	fputs(_(" -c, --ctty     set the controlling terminal to the current one\n"), out);
+	fputs(_(" -w, --wait     wait program to exit, and use the same return\n"), out);
 
 	fputs(USAGE_HELP, out);
 	fputs(USAGE_VERSION, out);
@@ -43,9 +47,12 @@ int main(int argc, char **argv)
 {
 	int ch;
 	int ctty = 0;
+	pid_t pid;
+	int status = 0;
 
 	static const struct option longopts[] = {
 		{"ctty", no_argument, NULL, 'c'},
+		{"wait", no_argument, NULL, 'w'},
 		{"version", no_argument, NULL, 'V'},
 		{"help", no_argument, NULL, 'h'},
 		{NULL, 0, NULL, 0}
@@ -56,13 +63,16 @@ int main(int argc, char **argv)
 	textdomain(PACKAGE);
 	atexit(close_stdout);
 
-	while ((ch = getopt_long(argc, argv, "+Vhc", longopts, NULL)) != -1)
+	while ((ch = getopt_long(argc, argv, "+Vhcw", longopts, NULL)) != -1)
 		switch (ch) {
 		case 'V':
 			printf(UTIL_LINUX_VERSION);
 			return EXIT_SUCCESS;
 		case 'c':
 			ctty=1;
+			break;
+		case 'w':
+			status = 1;
 			break;
 		case 'h':
 			usage(stdout);
@@ -74,7 +84,8 @@ int main(int argc, char **argv)
 		usage(stderr);
 
 	if (getpgrp() == getpid()) {
-		switch (fork()) {
+		pid = fork();
+		switch (pid) {
 		case -1:
 			err(EXIT_FAILURE, _("fork"));
 		case 0:
@@ -82,7 +93,13 @@ int main(int argc, char **argv)
 			break;
 		default:
 			/* parent */
-			return 0;
+			if (!status)
+				return EXIT_SUCCESS;
+			if (wait(&status) != pid)
+				err(EXIT_FAILURE, "wait");
+			if (WIFEXITED(status))
+				return WEXITSTATUS(status);
+			err(status, _("child %d did not exit normally"), pid);
 		}
 	}
 	if (setsid() < 0)
