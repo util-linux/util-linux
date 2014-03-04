9a52e7035630bf84be65823a2fed4fc452de13a5
Stewart Smith <stewart@linux.vnet.ibm.com>
2014-03-04 15:27:27 +1100

    lscpu: don't assume filesystem supports d_type when searching for NUMA nodes
    Not all file systems support the d_type field and simply checking for
    d_type == DT_DIR in is_node_dirent would cause the test suite to fail
    if run on (for example) XFS.
    
    The simple fix is to check for DT_DIR or DT_UNKNOWN in is_node_dirent.
    
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- sys-utils/lscpu.c
+++ sys-utils/lscpu.c
@@ -919,7 +919,7 @@ static inline int is_node_dirent(struct dirent *d)
 	return
 		d &&
 #ifdef _DIRENT_HAVE_D_TYPE
-		d->d_type == DT_DIR &&
+		(d->d_type == DT_DIR || d->d_type == DT_UNKNOWN) &&
 #endif
 		strncmp(d->d_name, "node", 4) == 0 &&
 		isdigit_string(d->d_name + 4);
