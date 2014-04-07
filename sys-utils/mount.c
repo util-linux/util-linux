5f148289adf1f390c3e5f8fce73dd2a13eb3b6f8
Karel Zak <kzak@redhat.com>
2014-04-07 11:53:05 +0200

    mount: fix --all and nofail return code
    Now the "nofail" affects warnings warning messages only. That's wrong
    and regression (against original non-libmount version). The nofail has
    to control return code too.
    
    Reported-by: Patrick McLean <chutzpah@gentoo.org>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- sys-utils/mount.c
+++ sys-utils/mount.c
@@ -197,12 +197,14 @@ static int mount_all(struct libmnt_context *cxt)
 			if (mnt_context_is_verbose(cxt))
 				printf("%-25s: mount successfully forked\n", tgt);
 		} else {
-			mk_exit_code(cxt, mntrc);	/* to print warnings */
-
-			if (mnt_context_get_status(cxt)) {
+			if (mk_exit_code(cxt, mntrc) == MOUNT_EX_SUCCESS) {
 				nsucc++;
 
-				if (mnt_context_is_verbose(cxt))
+				/* Note that MOUNT_EX_SUCCESS return code does
+				 * not mean that FS has been really mounted
+				 * (e.g. nofail option) */
+				if (mnt_context_get_status(cxt) 
+				    && mnt_context_is_verbose(cxt))
 					printf("%-25s: successfully mounted\n", tgt);
 			} else
 				nerrs++;
