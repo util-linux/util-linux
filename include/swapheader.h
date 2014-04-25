d38b55375f7fceda03544c355e4b5350100029db
Sami Kerola <kerolasa@iki.fi>
2014-04-25 22:08:23 +0100

    include/swapheader.h: ensure type sizes
    Use consistently the same type sizes as in libblkid and kernel.
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- include/swapheader.h
+++ include/swapheader.h
@@ -5,15 +5,17 @@
 #define SWAP_UUID_LENGTH 16
 #define SWAP_LABEL_LENGTH 16
 
+#include <stdint.h>
+
 struct swap_header_v1_2 {
 	char	      bootbits[1024];    /* Space for disklabel etc. */
-	unsigned int  version;
-	unsigned int  last_page;
-	unsigned int  nr_badpages;
+	uint32_t      version;
+	uint32_t      last_page;
+	uint32_t      nr_badpages;
 	unsigned char uuid[SWAP_UUID_LENGTH];
 	char	      volume_name[SWAP_LABEL_LENGTH];
-	unsigned int  padding[117];
-	unsigned int  badpages[1];
+	uint32_t      padding[117];
+	uint32_t      badpages[1];
 };
 
 #define SWAP_HEADER_SIZE (sizeof(struct swap_header_v1_2))
