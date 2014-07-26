54544d369b65a2dd1c25421d4f82b9321879b461
Lauri Nurmi <lanurmi@iki.fi>
2014-07-26 12:56:20 +0300

    lsblk: use pluralized translation.

--- misc-utils/lsblk.c
+++ misc-utils/lsblk.c
@@ -205,9 +205,11 @@ static int ncolumns;
 static inline size_t err_columns_index(size_t arysz, size_t idx)
 {
 	if (idx >= arysz)
-		errx(EXIT_FAILURE, _("too many columns specified, "
-				     "the limit is %zu columns."),
-				arysz - 1);
+		errx(EXIT_FAILURE, P_("too many columns specified, "
+				     "the limit is %zu column.",
+				     "too many columns specified, "
+				     "the limit is %zu columns.", arysz - 1),
+				     arysz - 1);
 	return idx;
 }
 
