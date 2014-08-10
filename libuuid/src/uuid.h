5f10333ab418efcc45605a4d961e2985cea5b8ce
Sami Kerola <kerolasa@iki.fi>
2014-08-10 16:36:09 +0100

    libuuid: add extern qualifiers to uuid/uuid.h system header
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- libuuid/src/uuid.h
+++ libuuid/src/uuid.h
@@ -67,35 +67,35 @@ extern "C" {
 #endif
 
 /* clear.c */
-void uuid_clear(uuid_t uu);
+extern void uuid_clear(uuid_t uu);
 
 /* compare.c */
-int uuid_compare(const uuid_t uu1, const uuid_t uu2);
+extern int uuid_compare(const uuid_t uu1, const uuid_t uu2);
 
 /* copy.c */
-void uuid_copy(uuid_t dst, const uuid_t src);
+extern void uuid_copy(uuid_t dst, const uuid_t src);
 
 /* gen_uuid.c */
-void uuid_generate(uuid_t out);
-void uuid_generate_random(uuid_t out);
-void uuid_generate_time(uuid_t out);
-int uuid_generate_time_safe(uuid_t out);
+extern void uuid_generate(uuid_t out);
+extern void uuid_generate_random(uuid_t out);
+extern void uuid_generate_time(uuid_t out);
+extern int uuid_generate_time_safe(uuid_t out);
 
 /* isnull.c */
-int uuid_is_null(const uuid_t uu);
+extern int uuid_is_null(const uuid_t uu);
 
 /* parse.c */
-int uuid_parse(const char *in, uuid_t uu);
+extern int uuid_parse(const char *in, uuid_t uu);
 
 /* unparse.c */
-void uuid_unparse(const uuid_t uu, char *out);
-void uuid_unparse_lower(const uuid_t uu, char *out);
-void uuid_unparse_upper(const uuid_t uu, char *out);
+extern void uuid_unparse(const uuid_t uu, char *out);
+extern void uuid_unparse_lower(const uuid_t uu, char *out);
+extern void uuid_unparse_upper(const uuid_t uu, char *out);
 
 /* uuid_time.c */
-time_t uuid_time(const uuid_t uu, struct timeval *ret_tv);
-int uuid_type(const uuid_t uu);
-int uuid_variant(const uuid_t uu);
+extern time_t uuid_time(const uuid_t uu, struct timeval *ret_tv);
+extern int uuid_type(const uuid_t uu);
+extern int uuid_variant(const uuid_t uu);
 
 #ifdef __cplusplus
 }
