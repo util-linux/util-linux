94b1623be345d8a86c31fdbeb5e4749fe98f5c9d
Sami Kerola <kerolasa@iki.fi>
2014-09-14 11:12:58 +0100

    renice: reorder usage() option descriptions
    Make the Usage: and Options: sections to be in same order, which I found
    to be quicker to use than alphabetical order.
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- sys-utils/renice.c
+++ sys-utils/renice.c
@@ -63,12 +63,13 @@ static void __attribute__((__noreturn__)) usage(FILE *out)
 		" %1$s [-n] <priority>  -u|--user <user>...\n"),
 		program_invocation_short_name);
 	fputs(USAGE_OPTIONS, out);
-	fputs(_(" -g, --pgrp <id>        interpret argument as process group ID\n"
-		" -n, --priority <num>   specify the nice increment value\n"
-		" -p, --pid <id>         interpret argument as process ID (default)\n"
-		" -u, --user <name|id>   interpret argument as username or user ID\n"
-		" -h, --help             display help text and exit\n"
-		" -V, --version          display version information and exit\n"), out);
+	fputs(_(" -n, --priority <num>   specify the nice increment value\n"), out);
+	fputs(_(" -p, --pid <id>         interpret argument as process ID (default)\n"), out);
+	fputs(_(" -g, --pgrp <id>        interpret argument as process group ID\n"), out);
+	fputs(_(" -u, --user <name|id>   interpret argument as username or user ID\n"), out);
+	fputs(USAGE_SEPARATOR, out);
+	fputs(USAGE_HELP, out);
+	fputs(USAGE_VERSION, out);
 	fprintf(out, USAGE_MAN_TAIL("renice(1)"));
 	exit(out == stderr ? EXIT_FAILURE : EXIT_SUCCESS);
 }
