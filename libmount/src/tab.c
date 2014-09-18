aedbde42432f5d774bb0adb363c5d668423b42ec
Andreas Henriksson <andreas@fatal.se>
2014-09-19 01:36:33 +0200

    libmount: only invoke loopcxt on linux
    This is part of an attempt to make libmount buildable on non-linux.
    
    The support for /dev/loop* is Linux-specific so just disable
    it on non-linux for now.
    
    Signed-off-by: Andreas Henriksson <andreas@fatal.se>

--- libmount/src/tab.c
+++ libmount/src/tab.c
@@ -1439,8 +1439,10 @@ int mnt_table_is_fs_mounted(struct libmnt_table *tb, struct libmnt_fs *fstab_fs)
 			} else
 				flags = LOOPDEV_FL_OFFSET;
 
+#if __linux__
 			if (loopdev_is_used(mnt_fs_get_srcpath(fs), src, offset, flags))
 				break;
+#endif
 		}
 
 		if (root) {
