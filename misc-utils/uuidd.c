fd346daf89860391f0687d76ddb2b1506ec15cdc
Sami Kerola <kerolasa@iki.fi>
2015-10-03 19:31:07 +0100

    uuidd: fix shadow declaration
    misc-utils/uuidd.c:384:13: warning: declaration of 'ret' shadows a previous
    			   local [-Wshadow]
    misc-utils/uuidd.c:327:6: note: shadowed declaration is here
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- misc-utils/uuidd.c
+++ misc-utils/uuidd.c
@@ -381,15 +381,15 @@ static void server_loop(const char *socket_path, const char *pidfile_path,
 
 #ifdef HAVE_LIBSYSTEMD
 	if (uuidd_cxt->no_sock) {
-		const int ret = sd_listen_fds(0);
+		const int r = sd_listen_fds(0);
 
-		if (ret < 0) {
-			errno = ret * -1;
+		if (r < 0) {
+			errno = r * -1;
 			err(EXIT_FAILURE, _("sd_listen_fds() failed"));
-		} else if (ret == 0)
+		} else if (r == 0)
 			errx(EXIT_FAILURE,
 			     _("no file descriptors received, check systemctl status uuidd.socket"));
-		else if (1 < ret)
+		else if (1 < r)
 			errx(EXIT_FAILURE,
 			     _("too many file descriptors received, check uuidd.socket"));
 		s = SD_LISTEN_FDS_START + 0;
