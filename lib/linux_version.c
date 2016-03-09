d404065a82187c8e71cf82e95ab503ff00608722
Karel Zak <kzak@redhat.com>
2016-03-09 11:49:42 +0100

    lib/linux_version: avoid major and minor shadowing
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- lib/linux_version.c
+++ lib/linux_version.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <sys/utsname.h>
 
+#include "c.h"
 #include "linux_version.h"
 
 int get_linux_version (void)
 {
 	static int kver = -1;
 	struct utsname uts;
-	int major = 0;
-	int minor = 0;
-	int teeny = 0;
+	int x = 0, y = 0, z = 0;
 	int n;
 
 	if (kver != -1)
 		return kver;
-	if (uname (&uts))
+	if (uname(&uts))
 		return kver = 0;
 
-	n = sscanf(uts.release, "%d.%d.%d", &major, &minor, &teeny);
+	n = sscanf(uts.release, "%d.%d.%d", &x, &y, &z);
 	if (n < 1 || n > 3)
 		return kver = 0;
 
-	return kver = KERNEL_VERSION(major, minor, teeny);
+	return kver = KERNEL_VERSION(x, y, y);
 }
