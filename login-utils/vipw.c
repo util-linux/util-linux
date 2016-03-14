11b86e17332be81c04674eea6d8b20922d0bfe54
Karel Zak <kzak@redhat.com>
2016-03-14 16:02:06 +0100

    vipw: use puts() when formatting unnecessary
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- login-utils/vipw.c
+++ login-utils/vipw.c
@@ -343,9 +343,10 @@ int main(int argc, char *argv[])
 	if (access(orig_file, F_OK) == 0) {
 		char response[80];
 
-		printf("%s", (program == VIGR)
+		puts((program == VIGR)
 		       ? _("You are using shadow groups on this system.\n")
 		       : _("You are using shadow passwords on this system.\n"));
+
 		/* TRANSLATORS: this program uses for y and n rpmatch(3),
 		 * which means they can be translated. */
 		printf(_("Would you like to edit %s now [y/n]? "), orig_file);
