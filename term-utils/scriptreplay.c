f0b3b904c7972374e8b9a280164508e356e9b5be
Sami Kerola <kerolasa@iki.fi>
2016-05-01 23:56:05 +0100

    scriptreplay: avoid re-implementing strtod_or_err()
    And use isnan() to detect NaN.
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- term-utils/scriptreplay.c
+++ term-utils/scriptreplay.c
@@ -30,6 +30,7 @@
 
 #include "closestream.h"
 #include "nls.h"
+#include "strutils.h"
 #include "c.h"
 
 #define SCRIPT_MIN_DELAY 0.0001		/* from original sripreplay.pl */
@@ -60,21 +61,11 @@ usage(FILE *out)
 static double
 getnum(const char *s)
 {
-	double d;
-	char *end;
+	const double d = strtod_or_err(s, _("failed to parse number"));
 
-	errno = 0;
-	d = strtod(s, &end);
-
-	if (end && *end != '\0')
-		errx(EXIT_FAILURE, _("expected a number, but got '%s'"), s);
-
-	if ((d == HUGE_VAL || d == -HUGE_VAL) && ERANGE == errno)
-		err(EXIT_FAILURE, _("divisor '%s'"), s);
-
-	if (!(d==d)) { /* did they specify "nan"? */
+	if (isnan(d)) {
 		errno = EINVAL;
-		err(EXIT_FAILURE, _("divisor '%s'"), s);
+		err(EXIT_FAILURE, "%s: %s", _("failed to parse number"), s);
 	}
 	return d;
 }
