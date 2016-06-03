92312207e718bccce377dd24f3a1ad1720768354
Karel Zak <kzak@redhat.com>
2016-06-03 14:41:04 +0200

    include/widechar: add missing fallbacks
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/widechar.h
+++ include/widechar.h
@@ -14,26 +14,32 @@
 # define wchar_t char
 # define wint_t int
 # define WEOF EOF
-  /* Fallback for input operations */
+
+ /* Fallback for input operations */
 # define fgetwc fgetc
 # define getwc getc
 # define getwchar getchar
 # define fgetws fgets
+
   /* Fallback for output operations */
 # define fputwc fputc
 # define putwc putc
 # define putwchar putchar
 # define fputws fputs
+
   /* Fallback for character classification */
 # define iswgraph isgraph
 # define iswprint isprint
 # define iswspace isspace
+
   /* Fallback for string functions */
 # define wcschr strchr
 # define wcsdup strdup
 # define wcslen strlen
 # define wcspbrk strpbrk
 
-# define wcwidth(c) 1
+# define wcwidth(c) (1)
+# define wmemset memset
+# define ungetwc ungetc
 
 #endif /* HAVE_WIDECHAR */
