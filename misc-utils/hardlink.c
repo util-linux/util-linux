c11af66f0fce8ded25f92c8dde294afb2a64b735
Francisco Javier Tsao Santín <tsao@gpul.org>
2016-07-10 03:57:43 +0200

    spec file reflects the atomic hardlinking patch; removed cleaning buildroot (redundant); current FSF address at .c source file

--- misc-utils/hardlink.c
+++ misc-utils/hardlink.c
@@ -12,10 +12,9 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
 
-   You should have received a copy of the GNU General Public
-   License along with this program; see the file COPYING.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License 
+   along with this program; if not, write to the Free Software Foundation, 
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA */
 
 /*  Changes by Rémy Card to use constants and add option -n.  */
 /*  Changes by Jindrich Novy to add option -h, -f, replace mmap(2), fix overflows */
