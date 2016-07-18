f1f5f21ee664c232df1454a322f2570301ee1add
Sami Kerola <kerolasa@iki.fi>
2016-07-18 21:49:27 +0100

    logger: remove trailing spaces when outputing to journal
    Issues:
    1. Whitespace-ish \r is not stripped, while it should be.
    2. In journal \r is considered unprintable.
    
    Lennart: "it is the duty of the client side to drop the trailing whitespace,
    which "logger" doesn't do".
    
    Reported-by: Ivan Babrou <ibobrik@gmail.com>
    Explained-by: Lennart Poettering <lennart@poettering.net>
    Reference: https://github.com/systemd/systemd/issues/3416
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- misc-utils/logger.c
+++ misc-utils/logger.c
@@ -341,13 +341,10 @@ static int journald_entry(struct logger_ctl *ctl, FILE *fp)
 	for (lines = 0; /* nothing */ ; lines++) {
 		buf = NULL;
 		sz = getline(&buf, &dummy, fp);
-		if (sz == -1) {
+		if (sz == -1 ||
+		   (sz = rtrim_whitespace((unsigned char *) buf)) == 0) {
 			free(buf);
 			break;
-		}
-		if (0 < sz && buf[sz - 1] == '\n') {
-			sz--;
-			buf[sz] = '\0';
 		}
 		if (lines == vectors) {
 			vectors *= 2;
