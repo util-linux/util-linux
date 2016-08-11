0773ad14a371a00a2191aa6527c0f80cca3ed6a7
Franco Fichtner <franco@opnsense.org>
2016-08-11 15:03:50 +0200

    pathnames: guard clashing definitions on FreeBSD

--- include/pathnames.h
+++ include/pathnames.h
@@ -46,10 +46,14 @@
 #define	_PATH_MAILDIR		"/var/spool/mail"
 #endif
 #define	_PATH_MOTDFILE		"/etc/motd"
+#ifndef _PATH_NOLOGIN
 #define	_PATH_NOLOGIN		"/etc/nologin"
+#endif
 #define	_PATH_VAR_NOLOGIN	"/var/run/nologin"
 
+#ifndef _PATH_LOGIN
 #define _PATH_LOGIN		"/bin/login"
+#endif
 #define _PATH_SHUTDOWN		"/sbin/shutdown"
 
 #define _PATH_TERMCOLORS_DIRNAME "terminal-colors.d"
