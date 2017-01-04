e33da967ef86a08087a42477485bcf72dca0acd8
Karel Zak <kzak@redhat.com>
2017-01-04 15:42:02 +0100

    lib/linux_version: fix code indention
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- lib/linux_version.c
+++ lib/linux_version.c
@@ -25,47 +25,47 @@ int get_linux_version (void)
 
 #ifdef TEST_PROGRAM_LINUXVERSION
 # include <stdlib.h>
-	int main(int argc, char *argv[])
-	{
-		int rc = EXIT_FAILURE;
+int main(int argc, char *argv[])
+{
+	int rc = EXIT_FAILURE;
 
-		if (argc == 1) {
-			printf("Linux version: %d\n", get_linux_version());
-			rc = EXIT_SUCCESS;
+	if (argc == 1) {
+		printf("Linux version: %d\n", get_linux_version());
+		rc = EXIT_SUCCESS;
 
-		} else if (argc == 5) {
-			const char *oper = argv[1];
+	} else if (argc == 5) {
+		const char *oper = argv[1];
 
-			int x = atoi(argv[2]),
-			    y = atoi(argv[3]),
-			    z = atoi(argv[4]);
-			int kver = get_linux_version();
-			int uver = KERNEL_VERSION(x, y, z);
+		int x = atoi(argv[2]),
+		    y = atoi(argv[3]),
+		    z = atoi(argv[4]);
+		int kver = get_linux_version();
+		int uver = KERNEL_VERSION(x, y, z);
 
-			if (strcmp(oper, "==") == 0)
-				rc = kver == uver;
-			else if (strcmp(oper, "<=") == 0)
-				rc = kver <= uver;
-			else if (strcmp(oper, ">=") == 0)
-				rc = kver >= uver;
-			else
-				errx(EXIT_FAILURE, "unsupported operator");
+		if (strcmp(oper, "==") == 0)
+			rc = kver == uver;
+		else if (strcmp(oper, "<=") == 0)
+			rc = kver <= uver;
+		else if (strcmp(oper, ">=") == 0)
+			rc = kver >= uver;
+		else
+			errx(EXIT_FAILURE, "unsupported operator");
 
-			if (rc)
-				printf("match\n");
-			else
-				printf("not-match [%d %s %d, x.y.z: %d.%d.%d]\n",
-						kver, oper, uver, x, y, z);
+		if (rc)
+			printf("match\n");
+		else
+			printf("not-match [%d %s %d, x.y.z: %d.%d.%d]\n",
+					kver, oper, uver, x, y, z);
 
-			rc = rc ? EXIT_SUCCESS : EXIT_FAILURE;
+		rc = rc ? EXIT_SUCCESS : EXIT_FAILURE;
 
-		} else
-			 fprintf(stderr, "Usage:\n"
-					 "   %s [<oper> <x> <y> <z>]\n"
-					 "supported operators:\n"
-					 "   ==, <=, >=\n",
-					 program_invocation_short_name);
+	} else
+		 fprintf(stderr, "Usage:\n"
+				 "   %s [<oper> <x> <y> <z>]\n"
+				 "supported operators:\n"
+				 "   ==, <=, >=\n",
+				 program_invocation_short_name);
 
-		return rc;
-	}
+	return rc;
+}
 #endif
