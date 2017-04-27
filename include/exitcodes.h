79055689a2da1855c2d2fe090fec5b9399400d72
Karel Zak <kzak@redhat.com>
2017-04-27 14:26:45 +0200

    include/exitcodes: remove mount(8) exit codes
    All defined by libmount now.
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/exitcodes.h
+++ include/exitcodes.h
@@ -22,14 +22,4 @@
 #define FSCK_EX_USAGE		16	/* Usage or syntax error */
 #define FSCK_EX_LIBRARY		128	/* Shared library error */
 
-/* Exit codes used by mount-line programs */
-#define MOUNT_EX_SUCCESS	0	/* No errors */
-#define MOUNT_EX_USAGE		1	/* incorrect invocation or permission */
-#define MOUNT_EX_SYSERR		2	/* out of memory, cannot fork, ... */
-#define MOUNT_EX_SOFTWARE	4	/* internal mount bug or wrong version */
-#define MOUNT_EX_USER		8	/* user interrupt */
-#define MOUNT_EX_FILEIO		16	/* problems writing, locking, ... mtab/fstab */
-#define MOUNT_EX_FAIL		32	/* mount failure */
-#define MOUNT_EX_SOMEOK		64	/* some mount succeeded */
-
 #endif	/* UTIL_LINUX_EXITCODES_H */
