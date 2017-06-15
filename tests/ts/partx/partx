util-linux/util-linux

commit 2d47fa3921b86f16b07065a83e381708fa852d4a

partx: fix --nr usage
Reported-by: Serge van den Boom <serge+util-linux@vdboom.org>
Signed-off-by: Karel Zak <kzak@redhat.com>

Documentation/releases/v2.28-ReleaseNotes
@@ -41,7 +41,7 @@ The library libsmartcols has been massively improved to print table ranges,
 multi-line cells, table titles and to support continuous printing.
 
 The package build system and code have been improved to be more portable to
-non-Linux systems (BSD, XOs).
+non-Linux systems (BSD, OSX).
 
 The package does not provide fallback solutions for openat-family functions
 anymore.

disk-utils/partx.8
@@ -68,7 +68,7 @@ Do not use it in newly written scripts.
 Specify the range of partitions.  For backward compatibility also the
 format \fIM\fB\-\fIN\fR is supported.
 The range may contain negative numbers, for example
-.B \-\-nr :\-1
+.B \-\-nr -\1:\-1
 means the last partition, and
 .B \-\-nr \-2:\-1
 means the last two partitions.  Supported range specifications are:

disk-utils/partx.c
@@ -267,6 +267,30 @@ dflt:
 	return SLICES_MAX;
 }
 
+static int recount_range_by_pt(blkid_partlist ls, int *lower, int *upper)
+{
+	int n = 0, i, nparts = blkid_partlist_numof_partitions(ls);
+
+	for (i = 0; i < nparts; i++) {
+		blkid_partition par = blkid_partlist_get_partition(ls, i);
+		int partno = blkid_partition_get_partno(par);
+		n = max(partno, n);
+	}
+
+	if (*lower < 0)
+		*lower = n + *lower + 1;
+	if (*upper < 0)
+		*upper = n + *upper + 1;
+
+	if (*lower > *upper && *upper != 0) {
+		warnx(_("specified range <%d:%d> does not make sense"), *lower, *upper);
+		return -EINVAL;
+	}
+	if (verbose)
+		printf(_("range recount: max partno=%d, lower=%d, upper=%d\n"), n, *lower, *upper);
+	return 0;
+}
+
 static void del_parts_warnx(const char *device, int first, int last)
 {
 	if (first == last)
@@ -284,6 +308,7 @@ static int del_parts(int fd, const char *device, dev_t devno,
 	assert(fd >= 0);
 	assert(device);
 
+	/* recount range by information in /sys */
 	if (!lower)
 		lower = 1;
 	if (!upper || lower < 0 || upper < 0) {
@@ -343,11 +368,15 @@ static void add_parts_warnx(const char *device, int first, int last)
 static int add_parts(int fd, const char *device,
 		     blkid_partlist ls, int lower, int upper)
 {
-	int i, nparts, rc = 0, errfirst = 0, errlast = 0;
+	int i, nparts, rc, errfirst = 0, errlast = 0;
 
 	assert(fd >= 0);
 	assert(device);
 	assert(ls);
+
+	rc = recount_range_by_pt(ls, &lower, &upper);
+	if (rc)
+		return rc;
 
 	nparts = blkid_partlist_numof_partitions(ls);
 
@@ -430,6 +459,8 @@ static int upd_parts(int fd, const char *device, dev_t devno,
 	assert(device);
 	assert(ls);
 
+	/* recount range by information in /sys, if on disk number of
+	 * partitions is greater than in /sys the use on-disk limit */
 	nparts = blkid_partlist_numof_partitions(ls);
 	if (!lower)
 		lower = 1;
@@ -505,9 +536,13 @@ static int upd_parts(int fd, const char *device, dev_t devno,
 
 static int list_parts(blkid_partlist ls, int lower, int upper)
 {
-	int i, nparts;
+	int i, nparts, rc;
 
 	assert(ls);
+
+	rc = recount_range_by_pt(ls, &lower, &upper);
+	if (rc)
+		return rc;
 
 	nparts = blkid_partlist_numof_partitions(ls);
 
@@ -644,6 +679,10 @@ static int show_parts(blkid_partlist ls, int scols_flags, int lower, int upper)
 			goto done;
 		}
 	}
+
+	rc = recount_range_by_pt(ls, &lower, &upper);
+	if (rc)
+		goto done;
 
 	for (i = 0; i < nparts; i++) {
 		blkid_partition par = blkid_partlist_get_partition(ls, i);
@@ -880,6 +919,9 @@ int main(int argc, char **argv)
 		} else {
 			device = argv[optind];
 			wholedisk = xstrdup(argv[optind + 1]);
+
+			if (device && wholedisk && !startswith(device, wholedisk))
+				errx(EXIT_FAILURE, _("partition and disk name do not match"));
 		}
 	} else if (optind == argc - 1) {
 		/* passed only one arg (ie: /dev/sda3 or /dev/sda) */
@@ -964,18 +1006,6 @@ int main(int argc, char **argv)
 			ls = get_partlist(pr, wholedisk, type);
 
 		if (ls) {
-			int n = blkid_partlist_numof_partitions(ls);
-
-			if (lower < 0)
-				lower = n + lower + 1;
-			if (upper < 0)
-				upper = n + upper + 1;
-			if (lower > upper) {
-				warnx(_("specified range <%d:%d> "
-					"does not make sense"), lower, upper);
-				rc = -1, what = ACT_NONE;
-			}
-
 			switch (what) {
 			case ACT_SHOW:
 				rc = show_parts(ls, scols_flags, lower, upper);
@@ -988,6 +1018,7 @@ int main(int argc, char **argv)
 				break;
 			case ACT_UPD:
 				rc = upd_parts(fd, wholedisk, disk_devno, ls, lower, upper);
+				break;
 			case ACT_NONE:
 				break;
 			default:

lib/strutils.c
@@ -734,7 +734,7 @@ int parse_range(const char *str, int *lower, int *upper, int def)
 			return -1;
 
 		if (*end == ':' && !*(end + 1))		/* <M:> */
-			*upper = 0;
+			*upper = def;
 		else if (*end == '-' || *end == ':') {	/* <M:N> <M-N> */
 			str = end + 1;
 			end = NULL;

commit 3a951379a75d64fe418d679e51d9646144a23efb

tests: fix and refactor partx
Subtest delete-non-existent was broken because since 2d47fa39
option --nr was missing. It wasn't noticed because we did not
check stderr. Now we check all return values and output.

Signed-off-by: Ruediger Meier <ruediger.meier@ga-group.nl>

tests/expected/partx/partx-detect-parts
@@ -2,3 +2,4 @@ NR START    END SECTORS  SIZE NAME UUID
  1    32  33791   33760 16.5M      
  2 33792  67583   33792 16.5M      
  3 67584 102399   34816   17M      
+OK

tests/ts/partx/partx
@@ -31,16 +31,35 @@ ts_check_test_command "$TS_CMD_DELPART"
 ts_skip_nonroot
 ts_check_prog "xz"
 
+shopt -s nullglob
+
+function check_partition_count
+{
+	# this function needs shopt -s nullglob
+	local cnt_want=$1
+	local devname=$(basename $TS_DEVICE)
+	local parts=(/sys/block/${devname}/${devname}*)
+	local cnt_have=${#parts[@]}
+
+	if [ $cnt_have -eq $cnt_want ]; then
+		return 0
+	fi
+	echo "error: expected $cnt_want partitions, have $cnt_have" >&2
+	echo "${parts[@]}" >&2
+	return 1
+}
+
 mkdir -p $TS_OUTDIR/images-pt
-for img in $(ls $TS_IMGDIR/*.img.xz | sort); do
+for img in $TS_IMGDIR/*.img.xz; do
 	name=$(basename $img .img.xz)
 	outimg=$TS_OUTDIR/images-pt/${name}.img
 
 	ts_init_subtest $name
-
-	xz -dc $img > $outimg
-
-	$TS_CMD_PARTX $outimg &> $TS_OUTPUT
+	{
+		xz -dc $img > $outimg &&
+			$TS_CMD_PARTX $outimg ||
+			echo "failed: $?"
+	} >$TS_OUTPUT 2>&1
 	ts_finalize_subtest
 done
 
@@ -48,90 +67,123 @@ done
 ts_scsi_debug_init dev_size_mb=50
 
 ts_init_subtest "addpart"
-$TS_CMD_ADDPART ${TS_DEVICE} 1 0 1
-[ "$?" == 0 ] && echo OK &>$TS_OUTPUT || ts_die "Unable to add partition"a &>$TS_OUTPUT
+{
+	$TS_CMD_ADDPART ${TS_DEVICE} 1 0 1 &&
+		echo OK ||
+		echo "Unable to add partition"
+	check_partition_count 1
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 udevadm settle
 
 ts_init_subtest "delpart"
-$TS_CMD_DELPART ${TS_DEVICE} 1
-[ "$?" == 0 ] && echo OK >> $TS_OUTPUT 2>&1 || ts_die "Unable to remove partition" >> $TS_OUTPUT 2>&1
+{
+	$TS_CMD_DELPART ${TS_DEVICE} 1 &&
+		echo OK ||
+		echo "Unable to remove partition"
+	check_partition_count 0
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 ts_scsi_debug_rmmod
 
 # set global variable TS_DEVICE
 ts_scsi_debug_init dev_size_mb=50 num_parts=$PARTS
-DEVNAME=$(basename $TS_DEVICE)
 
 ts_init_subtest "detect-parts"
-$TS_CMD_PARTX --show $TS_DEVICE &> $TS_OUTPUT
+{
+	$TS_CMD_PARTX --show $TS_DEVICE &&
+		echo OK ||
+		echo "Unable to list partitions"
+	check_partition_count $PARTS
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 udevadm settle
 
 ts_init_subtest "delete-all"
 #delete partinfo
-$TS_CMD_PARTX --delete $TS_DEVICE &> $TS_OUTPUT
-[ $? -eq 0 ] && echo "partitions deleted" &> $TS_OUTPUT ||
-    echo "Unable to delete partitions on $TS_DEVICE" &> $TS_OUTPUT
-ls -d "/sys/block/${DEVNAME}/${DEVNAME}*" 2>/dev/null && ts_die
+{
+	$TS_CMD_PARTX --delete $TS_DEVICE &&
+		echo "partitions deleted" ||
+		echo "Unable to delete partitions on $TS_DEVICE"
+	check_partition_count 0
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 udevadm settle
 
 ts_init_subtest "add-all"
 #read TS_DEVICE and restore the partinfo
-$TS_CMD_PARTX --add $TS_DEVICE &> $TS_OUTPUT
-[ $(ls -d /sys/block/${DEVNAME}/${DEVNAME}* 2>/dev/null | wc -l) -eq $PARTS ] &&
-   echo "partitions added" >> $TS_OUTPUT 2>&1  ||
-   echo "Unable to add partitions for $TS_DEVICE" >> $TS_OUTPUT 2>&1
+{
+	$TS_CMD_PARTX --add $TS_DEVICE &&
+		echo "partitions added" ||
+		echo "Unable to add partitions for $TS_DEVICE"
+	check_partition_count $PARTS
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 udevadm settle
 
 ts_init_subtest "update-one"
 #read TS_DEVICE and update second partition
-$TS_CMD_PARTX --update ${TS_DEVICE}2 &> $TS_OUTPUT
-[ $(ls -d /sys/block/${DEVNAME}/${DEVNAME}* 2>/dev/null | wc -l) -eq $PARTS ] &&
-   echo "partitions updated" >> $TS_OUTPUT 2>&1  ||
-   echo "Unable to update 2nd partition for $TS_DEVICE" >> $TS_OUTPUT 2>&1
+{
+	$TS_CMD_PARTX --update ${TS_DEVICE}2 &&
+		echo "partitions updated" ||
+		echo "Unable to update 2nd partition for $TS_DEVICE"
+	check_partition_count $PARTS
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 udevadm settle
 
 ts_init_subtest "delete-one"
 #remove last partition only
-$TS_CMD_PARTX -d --nr -1 $TS_DEVICE >> $TS_OUTPUT 2>&1
-[ $(ls -d /sys/block/${DEVNAME}/${DEVNAME}* 2>/dev/null |
-wc -l) -eq $((PARTS-1)) ] &&
-    echo "last partition removed" >> $TS_OUTPUT 2>&1 ||
-    echo "Unable to remove a partition on $TS_DEVICE" >> $TS_OUTPUT 2>&1
+{
+	$TS_CMD_PARTX -d --nr -1 $TS_DEVICE &&
+		echo "last partition removed" ||
+		echo "Unable to remove a partition on $TS_DEVICE"
+	check_partition_count $((PARTS-1))
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 udevadm settle
 
 ts_init_subtest "delete-non-existent"
 #attempt to remove it again
-$TS_CMD_PARTX -d $PARTS $TS_DEVICE && echo "partx failed: removed non-existing partition" &>$TS_OUTPUT || echo "partx: OK" &>$TS_OUTPUT
+{
+	$TS_CMD_PARTX -d --nr $PARTS $TS_DEVICE &&
+		echo "partx failed: removed non-existing partition" ||
+		echo "partx: OK"
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 udevadm settle
 
 ts_init_subtest "add-existing"
 #try adding an existing partition
-$TS_CMD_PARTX -a --nr 1 $TS_DEVICE &>/dev/null && echo "partx failed: re-added an existing partition" &> $TS_OUTPUT || echo "partx: OK" &> $TS_OUTPUT
+{
+	$TS_CMD_PARTX -a --nr 1 $TS_DEVICE 2>/dev/null &&
+		echo "partx failed: re-added an existing partition" ||
+		echo "partx: OK"
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 udevadm settle
 
 ts_init_subtest "change-by-range"
 # {-a|-d} --nr 0 should handle all partitions
-$TS_CMD_PARTX -d --nr 0 $TS_DEVICE
-ls -d /sys/block/${DEVNAME}/${DEVNAME}* 2>/dev/null && echo "Unable to delete partitions on $TS_DEVICE" >> $TS_OUTPUT 2>&1 || echo "partitions deleted" >> $TS_OUTPUT 2>&1
-$TS_CMD_PARTX -a --nr 0 $TS_DEVICE
-[ $(ls -d /sys/block/${DEVNAME}/${DEVNAME}* 2>/dev/null | wc -l) -eq $PARTS ] && echo "partitions added" >> $TS_OUTPUT 2>&1 || echo "Failed to add $TS_DEVICE partitions" >> $TS_OUTPUT 2>&1
+{
+	$TS_CMD_PARTX -d --nr 0 $TS_DEVICE &&
+		echo "partitions deleted" ||
+		echo "Unable to delete partitions on $TS_DEVICE"
+	check_partition_count 0
+	$TS_CMD_PARTX -a --nr 0 $TS_DEVICE &&
+		echo "partitions added" ||
+		echo "Failed to add $TS_DEVICE partitions"
+	check_partition_count $PARTS
+} >$TS_OUTPUT 2>&1
 ts_finalize_subtest
 
 ts_finalize