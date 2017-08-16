feeeaca9677414ee7627750559e6b5273142ccb5
Karel Zak <kzak@redhat.com>
2017-08-16 11:55:57 +0200

    libblkid: fix compiler warning [-Wformat-truncation=]
    The MBR partition pseudo-UUID is generated from table ID and partition
    partno. The final UUID size limit is 37 bytes. The table ID has to be
    restricted to keep compiler happy (for MBR the table ID is 8 bytes
    as string).
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- libblkid/src/partitions/partitions.c
+++ libblkid/src/partitions/partitions.c
@@ -1368,7 +1368,7 @@ int blkid_partition_gen_uuid(blkid_partition par)
 	if (!par || !par->tab || !*par->tab->id)
 		return -1;
 
-	snprintf(par->uuid, sizeof(par->uuid), "%s-%02x",
+	snprintf(par->uuid, sizeof(par->uuid), "%.33s-%02x",
 			par->tab->id, par->partno);
 	return 0;
 }
