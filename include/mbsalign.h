a2c9f86b775d269305d6a74d73322dfaf5454613
Karel Zak <kzak@redhat.com>
2017-11-22 14:41:10 +0100

    lib/mbsalign: add mbs_invalid_encode()
    Like mbs_safe_encode(), but it does not care about control chars.
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/mbsalign.h
+++ include/mbsalign.h
@@ -57,4 +57,7 @@ extern char *mbs_safe_encode(const char *s, size_t *width);
 extern char *mbs_safe_encode_to_buffer(const char *s, size_t *width, char *buf, const char *safechars);
 extern size_t mbs_safe_encode_size(size_t bytes);
 
+extern char *mbs_invalid_encode(const char *s, size_t *width);
+extern char *mbs_invalid_encode_to_buffer(const char *s, size_t *width, char *buf);
+
 #endif /* UTIL_LINUX_MBSALIGN_H */
