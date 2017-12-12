b4db227582f1d0a14c7b63de8d57e6052b753d57
Karel Zak <kzak@redhat.com>
2017-12-12 11:38:17 +0100

    test_sha1: update helper
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- tests/helpers/test_sha1.c
+++ tests/helpers/test_sha1.c
@@ -7,22 +7,22 @@
 int main(void)
 {
 	int i, ret;
-	SHA1_CTX ctx;
-	unsigned char digest[SHA1LENGTH];
+	UL_SHA1_CTX ctx;
+	unsigned char digest[UL_SHA1LENGTH];
 	unsigned char buf[BUFSIZ];
 
-	SHA1Init( &ctx );
+	ul_SHA1Init( &ctx );
 
 	while(!feof(stdin) && !ferror(stdin)) {
 		ret = fread(buf, 1, sizeof(buf), stdin);
 		if (ret)
-			SHA1Update( &ctx, buf, ret );
+			ul_SHA1Update( &ctx, buf, ret );
 	}
 
 	fclose(stdin);
-	SHA1Final( digest, &ctx );
+	ul_SHA1Final( digest, &ctx );
 
-	for (i = 0; i < SHA1LENGTH; i++)
+	for (i = 0; i < UL_SHA1LENGTH; i++)
 		printf( "%02x", digest[i] );
 	printf("\n");
 	return 0;
