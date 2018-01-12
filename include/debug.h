7bd0c1ae18b1c9864b55c0362b737383e70f80c3
Karel Zak <kzak@redhat.com>
2018-01-12 13:46:49 +0100

    include/debug: improve debug message
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- include/debug.h
+++ include/debug.h
@@ -97,7 +97,7 @@ struct ul_debug_maskname {
 		if (lib ## _debug_mask) { \
 			if (getuid() != geteuid() || getgid() != getegid()) { \
 				lib ## _debug_mask |= __UL_DEBUG_FL_NOADDR; \
-				fprintf(stderr, "%d: %s: SUID executable: pointer addresses suppressed.\n", getpid(), # lib); \
+				fprintf(stderr, "%d: %s: don't print memory addresses (SUID executable).\n", getpid(), # lib); \
 			} \
 		} \
 		lib ## _debug_mask |= pref ## INIT; \
