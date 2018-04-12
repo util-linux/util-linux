dfd3bb8bc203177e180762d78d20f99b73847d6d
Karel Zak <kzak@redhat.com>
2018-04-12 11:32:17 +0200

    lscpu: Cortex-A12 merged into Cortex-A17
    References: https://community.arm.com/processors/b/blog/posts/arm-cortex-a17-cortex-a12-processor-update
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- sys-utils/lscpu-arm.c
+++ sys-utils/lscpu-arm.c
@@ -49,7 +49,7 @@ static const struct id_part arm_part[] = {
     { 0xc07, "Cortex-A7" },
     { 0xc08, "Cortex-A8" },
     { 0xc09, "Cortex-A9" },
-    { 0xc0d, "Cortex-A12" },
+    { 0xc0d, "Cortex-A17" },	/* Originally A12 */
     { 0xc0f, "Cortex-A15" },
     { 0xc0e, "Cortex-A17" },
     { 0xc14, "Cortex-R4" },
