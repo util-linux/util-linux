3b6ca044daf6a86c650b841c4005b2b6e68453fa
Karel Zak <kzak@redhat.com>
2018-07-18 09:41:46 +0200

    lib/pager: fix compiler warning [-Wrestrict]
    lib/pager.c: In function ‘pager_preexec’:
    lib/pager.c:148:12: warning: passing argument 2 to restrict-qualified parameter aliases with argument 4 [-Wrestrict]
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- lib/pager.c
+++ lib/pager.c
@@ -141,11 +141,13 @@ static void pager_preexec(void)
 	 * Work around bug in "less" by not starting it until we
 	 * have real input
 	 */
-	fd_set in;
+	fd_set in, ex;
 
 	FD_ZERO(&in);
 	FD_SET(STDIN_FILENO, &in);
-	select(1, &in, NULL, &in, NULL);
+	ex = in;
+
+	select(STDIN_FILENO + 1, &in, NULL, &ex, NULL);
 
 	if (setenv("LESS", "FRSX", 0) != 0)
 		warn(_("failed to set the %s environment variable"), "LESS");
