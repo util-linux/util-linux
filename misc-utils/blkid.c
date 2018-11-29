0ab08179171cf872cace11bf679e4a9eab7af687
Karel Zak <kzak@redhat.com>
2018-11-29 13:30:11 +0100

    blkid: fix usage()
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- misc-utils/blkid.c
+++ misc-utils/blkid.c
@@ -102,7 +102,7 @@ static void __attribute__((__noreturn__)) usage(void)
 	fputs(_(	" -O, --offset <offset>      probe at the given offset\n"), out);
 	fputs(_(	" -u, --usages <list>        filter by \"usage\" (e.g. -u filesystem,raid)\n"), out);
 	fputs(_(	" -n, --match-types <list>   filter by filesystem type (e.g. -n vfat,ext3)\n"), out);
-	fputs(_(        " -D, --no-part-details      don't print info from partition table\n"), out);
+	fputs(_(	" -D, --no-part-details      don't print info from partition table\n"), out);
 
 	fputs(USAGE_SEPARATOR, out);
 	printf(USAGE_HELP_OPTIONS(28));
