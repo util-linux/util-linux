e4c6816cc34dd611ccbe0633aa222bd6dbae2572
Karel Zak <kzak@redhat.com>
2019-05-23 15:32:59 +0200

    ipcs: check return value when read from /proc [coverity scan]
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- sys-utils/ipcutils.c
+++ sys-utils/ipcutils.c
@@ -22,9 +22,12 @@ int ipc_msg_get_limits(struct ipc_limits *lim)
 	    access(_PATH_PROC_IPC_MSGMNB, F_OK) == 0 &&
 	    access(_PATH_PROC_IPC_MSGMAX, F_OK) == 0) {
 
-		ul_path_read_s32(NULL, &lim->msgmni, _PATH_PROC_IPC_MSGMNI);
-		ul_path_read_s32(NULL, &lim->msgmnb, _PATH_PROC_IPC_MSGMNB);
-		ul_path_read_u64(NULL, &lim->msgmax, _PATH_PROC_IPC_MSGMAX);
+		if (ul_path_read_s32(NULL, &lim->msgmni, _PATH_PROC_IPC_MSGMNI) != 0)
+			return 1;
+		if (ul_path_read_s32(NULL, &lim->msgmnb, _PATH_PROC_IPC_MSGMNB) != 0)
+			return 1;
+		if (ul_path_read_u64(NULL, &lim->msgmax, _PATH_PROC_IPC_MSGMAX) != 0)
+			return 1;
 	} else {
 		struct msginfo msginfo;
 
