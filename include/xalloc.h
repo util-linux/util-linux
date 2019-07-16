0695fe6b119ad9ed62d9b4d63fcc8b7c979fc163
Sami Kerola <kerolasa@iki.fi>
2019-07-16 20:30:18 +0100

    include/xalloc: reindent
    Discussion: https://github.com/karelzak/util-linux/pull/825
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- include/xalloc.h
+++ include/xalloc.h
@@ -8,89 +8,101 @@
  */
 
 #ifndef UTIL_LINUX_XALLOC_H
-#define UTIL_LINUX_XALLOC_H
+# define UTIL_LINUX_XALLOC_H
 
-#include <stdlib.h>
-#include <string.h>
+# include <stdlib.h>
+# include <string.h>
 
-#include "c.h"
+# include "c.h"
 
-#ifndef XALLOC_EXIT_CODE
-# define XALLOC_EXIT_CODE EXIT_FAILURE
-#endif
+# ifndef XALLOC_EXIT_CODE
+#  define XALLOC_EXIT_CODE EXIT_FAILURE
+# endif
 
-static inline void __attribute__((__noreturn__))
-__err_oom(const char *file, unsigned int line)
+static inline
+  __attribute__((__noreturn__))
+  void __err_oom(const char *file, unsigned int line)
 {
 	err(XALLOC_EXIT_CODE, "%s: %u: cannot allocate memory", file, line);
 }
 
-#define err_oom()	__err_oom(__FILE__, __LINE__)
+# define err_oom()	__err_oom(__FILE__, __LINE__)
 
-static inline __ul_alloc_size(1) __ul_returns_nonnull
-void *xmalloc(const size_t size)
+static inline
+  __ul_alloc_size(1)
+  __ul_returns_nonnull
+  void *xmalloc(const size_t size)
 {
-        void *ret = malloc(size);
+	void *ret = malloc(size);
 
-        if (!ret && size)
-                err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
-        return ret;
+	if (!ret && size)
+		err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
+	return ret;
 }
 
-static inline __ul_alloc_size(2) __ul_returns_nonnull
-void *xrealloc(void *ptr, const size_t size)
+static inline
+  __ul_alloc_size(2)
+  __ul_returns_nonnull
+  void *xrealloc(void *ptr, const size_t size)
 {
-        void *ret = realloc(ptr, size);
+	void *ret = realloc(ptr, size);
 
-        if (!ret && size)
-                err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
-        return ret;
+	if (!ret && size)
+		err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
+	return ret;
 }
 
-static inline __ul_calloc_size(1, 2) __ul_returns_nonnull
-void *xcalloc(const size_t nelems, const size_t size)
+static inline
+  __ul_calloc_size(1, 2)
+  __ul_returns_nonnull
+  void *xcalloc(const size_t nelems, const size_t size)
 {
-        void *ret = calloc(nelems, size);
+	void *ret = calloc(nelems, size);
 
-        if (!ret && size && nelems)
-                err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
-        return ret;
+	if (!ret && size && nelems)
+		err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
+	return ret;
 }
 
-static inline char __attribute__((warn_unused_result)) __ul_returns_nonnull
-*xstrdup(const char *str)
+static inline
+  __attribute__((warn_unused_result))
+  __ul_returns_nonnull
+  char *xstrdup(const char *str)
 {
-        char *ret;
+	char *ret;
 
-        assert(str);
+	assert(str);
 
-        ret = strdup(str);
+	ret = strdup(str);
 
-        if (!ret)
-                err(XALLOC_EXIT_CODE, "cannot duplicate string");
-        return ret;
+	if (!ret)
+		err(XALLOC_EXIT_CODE, "cannot duplicate string");
+	return ret;
 }
 
-static inline char * __attribute__((warn_unused_result)) __ul_returns_nonnull
-xstrndup(const char *str, size_t size)
+static inline
+  __attribute__((warn_unused_result))
+  __ul_returns_nonnull
+  char *xstrndup(const char *str, size_t size)
 {
-        char *ret;
+	char *ret;
 
-        assert(str);
+	assert(str);
 
-        ret = strndup(str, size);
+	ret = strndup(str, size);
 
-        if (!ret)
-                err(XALLOC_EXIT_CODE, "cannot duplicate string");
-        return ret;
+	if (!ret)
+		err(XALLOC_EXIT_CODE, "cannot duplicate string");
+	return ret;
 }
 
-
-static inline int __attribute__ ((__format__(printf, 2, 3)))
-    xasprintf(char **strp, const char *fmt, ...)
+static inline
+  __attribute__((__format__(printf, 2, 3)))
+  int xasprintf(char **strp, const char *fmt, ...)
 {
 	int ret;
 	va_list args;
+
 	va_start(args, fmt);
 	ret = vasprintf(&(*strp), fmt, args);
 	va_end(args);
@@ -99,17 +111,20 @@ static inline int __attribute__ ((__format__(printf, 2, 3)))
 	return ret;
 }
 
-static inline int  __attribute__ ((__format__(printf, 2, 0)))
-xvasprintf(char **strp, const char *fmt, va_list ap)
+static inline
+  __attribute__((__format__(printf, 2, 0)))
+  int xvasprintf(char **strp, const char *fmt, va_list ap)
 {
 	int ret = vasprintf(&(*strp), fmt, ap);
+
 	if (ret < 0)
 		err(XALLOC_EXIT_CODE, "cannot allocate string");
 	return ret;
 }
 
-
-static inline char * __attribute__((warn_unused_result)) xgethostname(void)
+static inline
+  __attribute__((warn_unused_result))
+  char *xgethostname(void)
 {
 	char *name;
 	size_t sz = get_hostname_max() + 1;
@@ -124,4 +139,4 @@ static inline char * __attribute__((warn_unused_result)) xgethostname(void)
 	return name;
 }
 
-#endif
+#endif /* UTIL_LINUX_XALLOC_H */
