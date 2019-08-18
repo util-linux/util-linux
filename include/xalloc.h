5df087f89d3b4dada56093b294d3bfae957335ac
Sami Kerola <kerolasa@iki.fi>
2019-08-18 11:11:00 +0100

    include/xalloc: reindent function bodies to unify indentation
    The file used unusual 7 spaces indent step in some of the functions.
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- include/xalloc.h
+++ include/xalloc.h
@@ -33,11 +33,11 @@ __ul_alloc_size(1)
 __ul_returns_nonnull
 void *xmalloc(const size_t size)
 {
-        void *ret = malloc(size);
+	void *ret = malloc(size);
 
-        if (!ret && size)
-                err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
-        return ret;
+	if (!ret && size)
+		err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
+	return ret;
 }
 
 static inline
@@ -45,11 +45,11 @@ __ul_alloc_size(2)
 __ul_returns_nonnull
 void *xrealloc(void *ptr, const size_t size)
 {
-        void *ret = realloc(ptr, size);
+	void *ret = realloc(ptr, size);
 
-        if (!ret && size)
-                err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
-        return ret;
+	if (!ret && size)
+		err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
+	return ret;
 }
 
 static inline
@@ -57,11 +57,11 @@ __ul_calloc_size(1, 2)
 __ul_returns_nonnull
 void *xcalloc(const size_t nelems, const size_t size)
 {
-        void *ret = calloc(nelems, size);
+	void *ret = calloc(nelems, size);
 
-        if (!ret && size && nelems)
-                err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
-        return ret;
+	if (!ret && size && nelems)
+		err(XALLOC_EXIT_CODE, "cannot allocate %zu bytes", size);
+	return ret;
 }
 
 static inline
@@ -69,15 +69,13 @@ __attribute__((warn_unused_result))
 __ul_returns_nonnull
 char *xstrdup(const char *str)
 {
-        char *ret;
+	char *ret;
 
-        assert(str);
-
-        ret = strdup(str);
-
-        if (!ret)
-                err(XALLOC_EXIT_CODE, "cannot duplicate string");
-        return ret;
+	assert(str);
+	ret = strdup(str);
+	if (!ret)
+		err(XALLOC_EXIT_CODE, "cannot duplicate string");
+	return ret;
 }
 
 static inline
@@ -85,15 +83,13 @@ __attribute__((warn_unused_result))
 __ul_returns_nonnull
 char *xstrndup(const char *str, size_t size)
 {
-        char *ret;
+	char *ret;
 
-        assert(str);
-
-        ret = strndup(str, size);
-
-        if (!ret)
-                err(XALLOC_EXIT_CODE, "cannot duplicate string");
-        return ret;
+	assert(str);
+	ret = strndup(str, size);
+	if (!ret)
+		err(XALLOC_EXIT_CODE, "cannot duplicate string");
+	return ret;
 }
 
 
@@ -103,6 +99,7 @@ int xasprintf(char **strp, const char *fmt, ...)
 {
 	int ret;
 	va_list args;
+
 	va_start(args, fmt);
 	ret = vasprintf(&(*strp), fmt, args);
 	va_end(args);
@@ -116,6 +113,7 @@ __attribute__((__format__(printf, 2, 0)))
 int xvasprintf(char **strp, const char *fmt, va_list ap)
 {
 	int ret = vasprintf(&(*strp), fmt, ap);
+
 	if (ret < 0)
 		err(XALLOC_EXIT_CODE, "cannot allocate string");
 	return ret;
@@ -130,7 +128,6 @@ char *xgethostname(void)
 	size_t sz = get_hostname_max() + 1;
 
 	name = xmalloc(sizeof(char) * sz);
-
 	if (gethostname(name, sz) != 0) {
 		free(name);
 		return NULL;
