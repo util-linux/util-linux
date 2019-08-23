eebc9e4dc2d3c0de174511823eca74a4cd645bff
Patrick Steinhardt <ps@pks.im>
2019-08-23 15:32:57 +0200

    tests: (col) avoid hardcoding of errno string
    The col/multibyte test has a hardcoded error string as part of its
    expected output that is returned by glibc's strerror(3P) function. Even
    though many of these strings are the same across libc implementations,
    they are not standardiced and some are certainly different. One example
    is the string for EILSEQ on musl libc.
    
    To fix this, we introduce a new test helper "test_strerror". The helper
    can be invoked with an error code like "EILSEQ", which will cause it to
    print out the the respective error message for that code. Note that
    "test_strerror" cannot act on the error's value (e.g. 84 for EILSEQ), as
    these aren't standardized either. Instead, we thus need to have an array
    of the error's string representation ("EILSEQ") to its respective error
    code (the define EILSEQ). The array can trivially be extended as
    required, thus it is only sparsely populated with EILSEQ right now.
    
    To fix the col/multibyte test, we introduce a call to sed(1) to replace
    the strerror(3P) message from EILSEQ with "EILSEQ". Furthermore, as
    we're running tests with the POSIX locale by default which treats all
    bytes as valid multibyte sequences, we have to change to the C.UTF-8
    locale instead to actually get an error.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>

---
+++ tests/helpers/test_strerror.c
@@ -0,0 +1,42 @@
+/*
+ * This test program prints errno messages to allow for portable
+ * verification of error messages.
+ *
+ * Copyright (C) 2019 Patrick Steinhardt <ps@pks.im
+ *
+ * This file may be redistributed under the terms of the GNU Public
+ * License.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define E(x) { #x, x }
+static struct {
+	const char *str;
+	int error;
+} errors[] = {
+	E(EILSEQ)
+};
+
+int main(int argc, const char *argv[])
+{
+	size_t i;
+
+	if (argc != 2) {
+		fprintf(stderr, "USAGE: %s <errno>\n", argv[0]);
+		return -1;
+	}
+
+	for (i = 0; i < sizeof(errors)/sizeof(*errors); i++) {
+		if (strcmp(errors[i].str, argv[1]))
+			continue;
+		puts(strerror(errors[i].error));
+		return 0;
+	}
+
+	fprintf(stderr, "Invalid errno: %s\n", argv[1]);
+	return -1;
+}
