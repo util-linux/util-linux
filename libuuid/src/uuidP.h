e0bea4dfa85d3759ca7c2b6da1de6b4ca67d63cf
Karel Zak <kzak@redhat.com>
2019-09-20 16:06:00 +0200

    libuuid: add header file guard [lgtm scan]
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- libuuid/src/uuidP.h
+++ libuuid/src/uuidP.h
@@ -31,6 +31,8 @@
  * DAMAGE.
  * %End-Header%
  */
+#ifndef _UUID_UUID_PRIVATE_H
+#define _UUID_UUID_PRIVATE_H
 
 #include <inttypes.h>
 #include <sys/types.h>
@@ -88,3 +90,5 @@ struct uuid {
  */
 void uuid_pack(const struct uuid *uu, uuid_t ptr);
 void uuid_unpack(const uuid_t in, struct uuid *uu);
+
+#endif /* _UUID_UUID_PRIVATE_H */
