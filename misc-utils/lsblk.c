a83bea2d3d8ca53a078ab23049b00ec93f6887bc
Karel Zak <kzak@redhat.com>
2020-10-12 13:19:35 +0200

    bash-completion: (lsblk) update columns
    and fix lsblk --help output
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- misc-utils/lsblk.c
+++ misc-utils/lsblk.c
@@ -1814,7 +1814,7 @@ static void __attribute__((__noreturn__)) usage(void)
 	fprintf(out, USAGE_COLUMNS);
 
 	for (i = 0; i < ARRAY_SIZE(infos); i++)
-		fprintf(out, " %11s  %s\n", infos[i].name, _(infos[i].help));
+		fprintf(out, " %12s  %s\n", infos[i].name, _(infos[i].help));
 
 	printf(USAGE_MAN_TAIL("lsblk(8)"));
 
