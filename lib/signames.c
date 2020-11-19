b24fa471728bf671017588e28da63989e3eccc75
Karel Zak <kzak@redhat.com>
2020-11-19 12:04:08 +0100

    lib/signames: change license to public domain
    The file is simple list of signal names and trivial function to search
    in the list. We do not need any extra license here and as public
    domain it will be more portable to other tools (GPL and BSD) in the
    util-linux.
    
    All previous contributors (Sami, Niklas and me) agree with this change.
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- lib/signames.c
+++ lib/signames.c
@@ -1,42 +1,11 @@
 /*
- * Copyright (c) 1988, 1993, 1994, 2017
- *  The Regents of the University of California.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *  This product includes software developed by the University of
- *  California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-/*
- *  2017-10-14 Niklas Hambüchen <mail@nh2.me>
- *  - Extracted signal names mapping from kill.c
- *
- * Copyright (C) 2014 Sami Kerola <kerolasa@iki.fi>
- * Copyright (C) 2014 Karel Zak <kzak@redhat.com>
- * Copyright (C) 2017 Niklas Hambüchen <mail@nh2.me>
+ * No copyright is claimed.  This code is in the public domain; do with
+ * it what you wish.
+
+ * Written by:
+ *  Sami Kerola <kerolasa@iki.fi>
+ *  Karel Zak <kzak@redhat.com>
+ *  Niklas Hambüchen <mail@nh2.me>
  */
 
 #include <ctype.h>		/* for isdigit() */
