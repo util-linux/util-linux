830a871e16d5eb1711914615467dc9d6a6e01764
Karel Zak <kzak@redhat.com>
2021-01-25 17:32:17 +0100

    github: cleanup cibuild.sh
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- .github/workflows/cibuild.sh
+++ .github/workflows/cibuild.sh
@@ -8,41 +8,34 @@ set -ex
 for phase in "${PHASES[@]}"; do
     case $phase in
 	CONFIGURE)
-		opts = "--disable-use-tty-group \
+		opts="--disable-use-tty-group \
 			--disable-makeinstall-chown \
 			--enable-all-programs \
 			--enable-asan \
 			--enable-ubsan \
+			--without-python \
 			--enable-werror"
 
 		if [[ "$COMPILER" == clang ]]; then
 			opts="$opts --enable-fuzzing-engine"
 		fi
 
-		echo "## CONFIGURE: git-clean"
 		sudo -E git clean -xdf
 
-		echo "## CONFIGURE: autogen.sh"
 		./autogen.sh
-
-		echo "## CONFIGURE: $opts --"
 		./configure $opts
 		;;
         MAKE)
-		echo "## MAKE"
 		make -j V=1
 		make -j check-programs V=1
 		;;
 	INSTALL)
-		echo "## MAKE INSTALL"
 		make install DESTDIR=/tmp/dest
 		;;
 	CHECK)
-		echo "## MAKE CHECK"
-		./tests/run.h --show-diff
+		./tests/run.sh --show-diff
 		;;
 	DISTCHECK)
-		echo "## MAKE DISTCHECK"
 		make distcheck
 		;;
 	
