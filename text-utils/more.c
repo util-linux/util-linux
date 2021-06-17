19f2cebd489786a81f499d45585494ab9f497f14
Karel Zak <kzak@redhat.com>
2021-06-17 12:21:04 +0200

    more: fix setuid/setgid order
    The rule is pretty simple, always use setgid() before setuid().
    
    Reported-by: Jan Pazdziora <jpazdziora@redhat.com>
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- text-utils/more.c
+++ text-utils/more.c
@@ -1250,10 +1250,10 @@ static void execute(struct more_control *ctl, char *filename, char *cmd, ...)
 		va_end(argp);
 
 		if (geteuid() != getuid() || getegid() != getgid()) {
-			if (setuid(getuid()) < 0)
-				err(EXIT_FAILURE, _("setuid failed"));
 			if (setgid(getgid()) < 0)
 				err(EXIT_FAILURE, _("setgid failed"));
+			if (setuid(getuid()) < 0)
+				err(EXIT_FAILURE, _("setuid failed"));
 		}
 
 		execvp(cmd, args);
