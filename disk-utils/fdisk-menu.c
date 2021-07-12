f05a5bb2c2e62d3753f8c9262b9f6cbf469e5b0e
Pali Rohár <pali.rohar@gmail.com>
2021-07-13 00:40:00 +0200

    fdisk: move reorder diag messages to fdisk_reorder_partitions()
    The function fdisk_reorder_partitions() is also used in sfdisk and cfdisk
    and these commands assume info/warn messages from the library. So move all
    messages from fdisk to fdisk_reorder_partitions().
    
    Signed-off-by: Pali Rohár <pali.rohar@gmail.com>

--- disk-utils/fdisk-menu.c
+++ disk-utils/fdisk-menu.c
@@ -636,17 +636,6 @@ static int generic_menu_cb(struct fdisk_context **cxt0,
 			break;
 		case 'f':
 			rc = fdisk_reorder_partitions(cxt);
-			switch (rc) {
-			default:
-				fdisk_warnx(cxt, _("Failed to fix partitions order."));
-				break;
-			case 0:
-				fdisk_info(cxt, _("Partitions order fixed."));
-				break;
-			case 1:
-				fdisk_info(cxt, _("Nothing to do. Ordering is correct already."));
-				break;
-			}
 			break;
 		case 'r':
 			rc = fdisk_enable_details(cxt, 0);
