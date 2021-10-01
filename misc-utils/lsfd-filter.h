ef70cdd96d79d3986268eaf6d96f948da5f49e1a
Masatake YAMATO <yamato@redhat.com>
2021-10-02 01:53:14 +0900

    lsfd: change the license of the filtering engine to LGPL
    This change is for moving the filtering engine to libsmartcols in the
    future.
    
    In addition to the change, I made myself a copyright holder of the
    code.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- misc-utils/lsfd-filter.h
+++ misc-utils/lsfd-filter.h
@@ -1,22 +1,11 @@
 /*
  * lsfd-filter.c - filtering engine for lsfd
  *
- * Copyright (C) 2021 Red Hat, Inc. All rights reserved.
- * Written by Masatake YAMATO <yamato@redhat.com>
+ * Copyright (C) 2021 Red Hat, Inc.
+ * Copyright (C) 2021 Masatake YAMATO <yamato@redhat.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it would be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * This file may be redistributed under the terms of the
+ * GNU Lesser General Public License.
  */
 #ifndef UTIL_LINUX_LSFD_FILTER_H
 #define UTIL_LINUX_LSFD_FILTER_H
