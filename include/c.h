b9a6df0f4361adf3b1c0b89d97f6bee843567273
Cristian Rodríguez <crodriguez@owncloud.com>
2022-01-30 13:27:01 +0000

    include: remove local offsetof
    offsetof must be provided as an implementation-defined macro in stddef.h
    as it is required by ANSI C.

--- include/c.h
+++ include/c.h
@@ -192,11 +192,6 @@
 # endif
 #endif
 
-
-#ifndef offsetof
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
-#endif
-
 /*
  * container_of - cast a member of a structure out to the containing structure
  * @ptr:	the pointer to the member.
