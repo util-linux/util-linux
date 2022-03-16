efea86773ef57245bf821c51811d89309eb37e9a
Andreas Hindborg <andreas.hindborg@wdc.com>
2022-03-16 09:39:05 +0100

    blkzone: Do not print zone capacity if not supported
    If `blkzone` is built against kernel headers that do not define
    `BLK_ZONE_REP_CAPACITY`, `blkzone report` will use zone size when printing zone
    capacity. This patch changes the behavior of `blockzone report` in this case to
    omit the capacity field instead of using the potentially wrong value.
    
    Signed-off-by: Andreas Hindborg <andreas.hindborg@wdc.com>

--- sys-utils/blkzone.c
+++ sys-utils/blkzone.c
@@ -298,11 +298,19 @@ static int blkzone_report(struct blkzone_control *ctl)
 
 			if (only_capacity_sum) {
 				capacity_sum += cap;
-			} else {
+			} else if (has_zone_capacity(zi)) {
 				printf(_("  start: 0x%09"PRIx64", len 0x%06"PRIx64
 					", cap 0x%06"PRIx64", wptr 0x%06"PRIx64
 					" reset:%u non-seq:%u, zcond:%2u(%s) [type: %u(%s)]\n"),
 					start, len, cap, (type == 0x1) ? 0 : wp - start,
+					entry->reset, entry->non_seq,
+					cond, condition_str[cond & (ARRAY_SIZE(condition_str) - 1)],
+					type, type_text[type]);
+			} else {
+				printf(_("  start: 0x%09"PRIx64", len 0x%06"PRIx64
+					", wptr 0x%06"PRIx64
+					" reset:%u non-seq:%u, zcond:%2u(%s) [type: %u(%s)]\n"),
+					start, len, (type == 0x1) ? 0 : wp - start,
 					entry->reset, entry->non_seq,
 					cond, condition_str[cond & (ARRAY_SIZE(condition_str) - 1)],
 					type, type_text[type]);
