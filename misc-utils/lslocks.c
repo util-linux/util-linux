9b725af7028163bd29d39da3b910dffc903107ee
Karel Zak <kzak@redhat.com>
2022-03-28 11:39:27 +0200

    lslocks: fix maj:min scanf
    Fixes: https://github.com/util-linux/util-linux/issues/1633
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- misc-utils/lslocks.c
+++ misc-utils/lslocks.c
@@ -230,7 +230,7 @@ static ino_t get_dev_inode(char *str, dev_t *dev)
 	unsigned int maj = 0, min = 0;
 	ino_t inum = 0;
 
-	if (sscanf(str, "%02x:%02x:%ju", &maj, &min, &inum) != 3)
+	if (sscanf(str, "%x:%x:%ju", &maj, &min, &inum) != 3)
 		errx(EXIT_FAILURE, _("failed to parse '%s'"), str);
 
 	*dev = (dev_t) makedev(maj, min);
