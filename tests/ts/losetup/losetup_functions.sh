2b1060737fcd2cb5a11fe17cee1f62804a3d477c
Thomas Weißschuh <thomas@t-8ch.de>
2023-01-22 14:07:58 +0000

    drop tests

--- tests/ts/losetup/losetup_functions.sh
+++
@@ -1,7 +0,0 @@
-
-function lo_print {
-	local lo=$1
-	echo "offset:    $( $TS_CMD_LOSETUP --list --raw -n -O OFFSET $lo )"
-	echo "sizelimit: $( $TS_CMD_LOSETUP --list --raw -n -O SIZELIMIT $lo )"
-	echo "size:      $( $TS_CMD_LSBLK -o SIZE -b -n -r $lo )"
-}
