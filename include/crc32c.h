2598e25fb68ff37182fdb026b52055f177ceb24d
Jeremy Linton <jeremy.linton@arm.com>
2023-04-14 18:07:29 -0500

    lib: Add crc32c function that can deal with holes
    XFS, and possibly other filesystems expect that the CRC field
    is excluded (or rather RAZ) during the CRC operation. Lets
    create a generic helper that is similar to the CRC32 version
    ul_crc32_exclude_offset() which computes the CRC while replacing
    exclude_len bytes of exclude_off with zeros.
    
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>

--- include/crc32c.h
+++ include/crc32c.h
@@ -9,5 +9,9 @@
 #include <stdint.h>
 
 extern uint32_t crc32c(uint32_t crc, const void *buf, size_t size);
+extern uint32_t ul_crc32c_exclude_offset(uint32_t crc, const unsigned char *buf,
+					 size_t size, size_t exclude_off,
+					 size_t exclude_len);
+
 
 #endif /* UL_CRC32C_H */
