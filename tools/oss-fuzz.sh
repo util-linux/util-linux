f6e7a21ce91df5a0051a47d175408ebc77b3d7b3
Thomas Weißschuh <thomas@t-8ch.de>
2023-04-15 10:57:02 +0200

    build-sys: try to always use 64bit time support on glibc
    Signed-off-by: Thomas Weißschuh <thomas@t-8ch.de>

--- tools/oss-fuzz.sh
+++ tools/oss-fuzz.sh
@@ -24,8 +24,17 @@ if [[ "$SANITIZER" == undefined ]]; then
     CXXFLAGS+=" $UBSAN_FLAGS"
 fi
 
+CONFIGURE_ARGS="--disable-all-programs --enable-libuuid --enable-libfdisk --enable-last --enable-fuzzing-engine --enable-libmount --enable-libblkid"
+
+LIBC_VERSION="$(dpkg -s libc6 | grep Version | cut -d' ' -f2)"
+
+# Ubuntu focal uses glibc 2.31 but 2.34 is necessary
+if dpkg --compare-versions "$LIBC_VERSION" 'lt' '2.34'; then
+	CONFIGURE_ARGS="$CONFIGURE_ARGS --disable-year2038"
+fi
+
 ./autogen.sh
-./configure --disable-all-programs --enable-libuuid --enable-libfdisk --enable-last --enable-fuzzing-engine --enable-libmount --enable-libblkid
+./configure $CONFIGURE_ARGS
 make -j$(nproc) V=1 check-programs
 
 for d in "$(dirname $0)"/../tests/ts/fuzzers/test_*_fuzz_files; do
