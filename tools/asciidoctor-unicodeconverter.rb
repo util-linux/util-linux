05c7d67dee09e36688b49da62ba381e36f3a9cf5
Thomas Weißschuh <thomas@t-8ch.de>
2023-05-10 12:21:24 +0200

    tools: (asciidoctor-unicodeconverter) properly handle dotted commands
    Split from the end of the docname, as otherwise commands like "fsck.foo"
    are stripped of their suffix.
    
    Signed-off-by: Thomas Weißschuh <thomas@t-8ch.de>

--- tools/asciidoctor-unicodeconverter.rb
+++ tools/asciidoctor-unicodeconverter.rb
@@ -12,7 +12,7 @@ module UnicodeConverter
     def process document, reader
       lines = reader.read_lines
       state = BEFORE_NAME_SECTION
-      command = document.attributes['docname'].split('.', 2)[0]
+      command = document.attributes['docname'].rpartition('.')[0]
       lines.map! do |line|
         if state == IN_NAME_SECTION
           line.sub! " \u2013 ", " - "
