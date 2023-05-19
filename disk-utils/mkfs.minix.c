873a896e02b50f6866439eaf9ce59e65ce8ceaa7
Thomas Weißschuh <thomas@t-8ch.de>
2023-05-19 16:47:22 +0200

    mkfs.minix: handle 64bit time on 32bit system
    Signed-off-by: Thomas Weißschuh <thomas@t-8ch.de>

--- disk-utils/mkfs.minix.c
+++ disk-utils/mkfs.minix.c
@@ -157,9 +157,9 @@ static void __attribute__((__noreturn__)) usage(void)
 static inline time_t mkfs_minix_time(time_t *t)
 {
 	const char *str = getenv("MKFS_MINIX_TEST_SECOND_SINCE_EPOCH");
-	time_t sec;
+	uint64_t sec;
 
-	if (str && sscanf(str, "%ld", &sec) == 1)
+	if (str && sscanf(str, "%"SCNd64, &sec) == 1)
 		return sec;
 	return time(t);
 }
