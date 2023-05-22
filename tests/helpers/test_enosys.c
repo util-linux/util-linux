f084562c36b7579a1570b5e4267bb0aa3c1116e8
Thomas Weißschuh <thomas@t-8ch.de>
2023-05-22 17:25:15 +0200

    enosys: split audit arch detection into dedicated header
    Signed-off-by: Thomas Weißschuh <thomas@t-8ch.de>

--- tests/helpers/test_enosys.c
+++ tests/helpers/test_enosys.c
@@ -27,55 +27,8 @@
 #include <sys/prctl.h>
 
 #include "c.h"
+#include "audit-arch.h"
 #include "exitcodes.h"
-
-#if __x86_64__
-#    define SECCOMP_ARCH_NATIVE AUDIT_ARCH_X86_64
-#elif __i386__
-#    define SECCOMP_ARCH_NATIVE AUDIT_ARCH_I386
-#elif __arm__
-#    define SECCOMP_ARCH_NATIVE AUDIT_ARCH_ARM
-#elif __aarch64__
-#    define SECCOMP_ARCH_NATIVE AUDIT_ARCH_AARCH64
-#elif __riscv
-#    if __riscv_xlen == 32
-#        define SECCOMP_ARCH_NATIVE AUDIT_ARCH_RISCV32
-#    elif __riscv_xlen == 64
-#        define SECCOMP_ARCH_NATIVE AUDIT_ARCH_RISCV64
-#    endif
-#elif __s390__
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_S390
-#elif __s390x__
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_S390X
-#elif __PPC64__
-#    if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_PPC64
-#    else
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_PPC64LE
-#    endif
-#else
-#    error Unknown target architecture
-#elif __powerpc__
-#    define SECCOMP_ARCH_NATIVE AUDIT_ARCH_PPC
-#elif __mips__
-#    if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_MIPS
-#    else
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_MIPSEL
-#    endif
-#elif __arc__
-#    if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_ARCV2BE
-#    else
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_ARCV2
-#    endif
-#elif __sparc__
-#    if __SIZEOF_POINTER__ == 4
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_SPARC
-#    else
-# 	 define SECCOMP_ARCH_NATIVE AUDIT_ARCH_SPARC64
-#    endif
-#endif
 
 #define syscall_nr (offsetof(struct seccomp_data, nr))
 
