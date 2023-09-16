dc9b6a3c5c325961f5e3fb453160bc98a0df0513
Thomas Weißschuh <thomas@t-8ch.de>
2023-09-16 12:03:06 +0200

    ci: run full testsuite under musl libc
    Signed-off-by: Thomas Weißschuh <thomas@t-8ch.de>

--- .github/workflows/openwrt-sdk-env.sh
+++ .github/workflows/openwrt-sdk-env.sh
@@ -7,11 +7,13 @@ STAGING_DIR="$(echo "$sdk"/staging_dir/toolchain-*)"
 . "$STAGING_DIR/info.mk"
 
 PATH="$sdk/staging_dir/host/bin:$PATH"
+LD_LIBRARY_PATH="$STAGING_DIR/lib"
 CC="$STAGING_DIR/bin/${TARGET_CROSS}gcc"
+DYNAMIC_LINKER="$(echo "$STAGING_DIR"/lib/ld-musl-*)"
 BISON_PKGDATADIR="$sdk/staging_dir/host/share/bison"
 M4="$sdk/staging_dir/host/bin/m4"
 HOST_TRIPLET="$("$CC" -dumpmachine)"
 
 echo "Building for $HOST_TRIPLET from $sdk"
 
-export STAGING_DIR PATH CC BISON_PKGDATADIR M4 HOST_TRIPLET
+export STAGING_DIR PATH LD_LIBRARY_PATH CC DYNAMIC_LINKER BISON_PKGDATADIR M4 HOST_TRIPLET
