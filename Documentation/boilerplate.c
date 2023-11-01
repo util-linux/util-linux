c14f5bf37adf681c378fc004f0996e0bcfd30ce0
Karel Zak <kzak@redhat.com>
2023-11-01 14:58:57 +0100

    docs: add SPDX to boilerplate.c
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- Documentation/boilerplate.c
+++ Documentation/boilerplate.c
@@ -2,25 +2,19 @@
  * util-linux package.
  * -- remove above */
 /*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
  * fixme-command-name - purpose of it
  *
  * Copyright (c) 20nn  Example Commercial, Inc
  * Written by Your Name <you@example.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 #include <getopt.h>
 #include <stdio.h>
 #include <unistd.h>
