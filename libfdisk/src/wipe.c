bbcb2b8bec02420acd55743860aba0a912e528a8
Thomas Weißschuh <thomas@t-8ch.de>
2023-11-29 19:51:49 +0100

    libfdisk: use new blkid_wipe_all helper
    Signed-off-by: Thomas Weißschuh <thomas@t-8ch.de>

--- libfdisk/src/wipe.c
+++ libfdisk/src/wipe.c
@@ -134,17 +134,8 @@ int fdisk_do_wipe(struct fdisk_context *cxt)
 			return rc;
 		}
 
-		blkid_probe_enable_superblocks(pr, 1);
-		blkid_probe_set_superblocks_flags(pr, BLKID_SUBLKS_MAGIC |
-						      BLKID_SUBLKS_BADCSUM);
-		blkid_probe_enable_partitions(pr, 1);
-		blkid_probe_set_partitions_flags(pr, BLKID_PARTS_MAGIC |
-				                     BLKID_PARTS_FORCE_GPT);
-
-		while (blkid_do_probe(pr) == 0) {
-			DBG(WIPE, ul_debugobj(wp, " wiping..."));
-			blkid_do_wipe(pr, FALSE);
-		}
+		DBG(WIPE, ul_debugobj(wp, " wiping..."));
+		blkid_wipe_all(pr);
 	}
 
 	blkid_free_probe(pr);
