e76e252cd66044e0be08a38c520cb1397a010d95
Phil Auld <pauld@redhat.com>
2024-03-26 13:24:07 -0400

    coreset:  Add a pull/push mode to copy the cookie from one running task to another.  Add a --dest <pid> argument for use with --copy to handle this.
    Suggested-by: Thijs Raymakers <thijs@raymakers.nl>
    Signed-off-by: Phul Auld <pauld@redhat.com>

--- schedutils/coreset.c
+++ schedutils/coreset.c
@@ -58,12 +58,14 @@ enum cmd_type {
 	CORE_CREATE,  /* PR_SCHED_CORE_CREATE */
 	CORE_PUSH,    /* PR_SCHED_CORE_SHARE_TO */
 	CORE_COPY,    /* PR_SCHED_CORE_SHARE_FROM */
+	CORE_COPYPUSH, /* SHARE_FROM and then SHARE_TO */
 };
 
 struct coreset {
 	pid_t		pid;		/* task PID (or tid) */
 	unsigned long   cookie;         /* storage for current cookie */
 	int             cmd;            /* what to do: one of cmd_type */
+	pid_t           dest;           /* only valid for COPYPUSH */
 	int             scope;          /* one of PR_SCHED_CORE_SCOPE_THREAD(0), PR_SCHED_CORE_SCOPE_THREAD_GROUP(1)
                                          * or PR_SCHED_CORE_SCOPE_PROCESS_GROUP  (2)
                                          */
@@ -82,11 +84,12 @@ static void __attribute__((__noreturn__)) usage(void)
 
 	fprintf(out, _(
 		"Options:\n"
-		" -c, --copy              coply the cookie from give pid to this cmd\n"
+		" -c, --copy              copy the cookie from give pid to this cmd (or dest pid)\n"
 		" -n, --new               create new cookie on pid or cmd\n"
 		" -t, --to                copy current task's cookie to existing pid or cmd\n"
 		" Absence of one of the mutually exclusive above options just reports current cookie on given pid (or cmd)\n"
 		" -p, --pid               operate on existing given pid/tid\n"
+		" -d, --dest              Use with -c, copy cookie from pid to dest_pid\n"
 		" -s, --scope             0, 1 or 2: apply change to task (0), thread group (1) or process group (2) of given pid/tid\n"
                 " Default scope is 0. Scope is ignored in some cases where it does not have an effect\n"
 		));
@@ -103,6 +106,8 @@ static void __attribute__((__noreturn__)) usage(void)
                 "    %1$s -s 1 -n -p 700\n"
 		"Copy cookie from existing task to new task:\n"
 		"    %1$s -c -p 700  sshd -b 1024\n"
+		"Copy cookie from existing task to a different existing task:\n"
+		"    %1$s -c -p 700 -d 12345\n"
 		"Clear cookie for all processes for given task (assuming current shell has no cookie):\n"
 		"    %1$s -s 2 -t -p 700\n"
 		"Note: pid can also be a tid as retrieved with the gettid(2) syscall.\n"),
@@ -118,11 +123,18 @@ static void __attribute__((__noreturn__)) usage(void)
 static void print_cookie(struct coreset *cs, int isnew)
 {
 	char *msg;
+	pid_t pid;
+	if (cs->cmd == CORE_COPY || !cs->pid)
+                pid = getpid(); /* with copy we want to report current's cookie */
+        else if (cs->cmd == CORE_COPYPUSH)
+                pid = cs->dest; /* with copypush we are changing dest's cookie */
+        else
+                pid = cs->pid;
 
 	msg = isnew ? _("pid %d's new cookie: 0x%0x\n") :
 		_("pid %d's current cookie: 0x%0x\n");
 
-	printf(msg, cs->pid ? cs->pid : getpid(), cs->cookie);
+	printf(msg, pid, cs->cookie);
 }
 
 static void __attribute__((__noreturn__)) err_cookie(pid_t pid, int set)
@@ -141,7 +153,13 @@ static void __attribute__((__noreturn__)) err_cookie(pid_t pid, int set)
 static unsigned long get_cookie(struct coreset *cs)
 {
 	unsigned long cookie;
-	pid_t pid = (cs->cmd == CORE_COPY ? 0: cs->pid); /* with copy we want to report current's cookie */
+	pid_t pid;
+	if (cs->cmd == CORE_COPY)
+		pid = 0; /* with copy we want to report current's cookie */
+	else if (cs->cmd == CORE_COPYPUSH)
+		pid = cs->dest; /* with copypush we are changing dest's cookie */
+	else 
+		pid = cs->pid; 
 
 	if (prctl(PR_SCHED_CORE, PR_SCHED_CORE_GET, pid, PR_SCHED_CORE_SCOPE_THREAD, &cookie) < 0 )
 		err_cookie(pid ? pid : getpid(), FALSE);
@@ -173,6 +191,13 @@ static void do_coreset(struct coreset *cs)
 		if (prctl(PR_SCHED_CORE, PR_SCHED_CORE_SHARE_TO, cs->pid, cs->scope, NULL) < 0)
 			err_cookie(cs->pid, cs->cmd);
 		break;
+	case CORE_COPYPUSH:
+		/* copy pid's cookie here then push it to dest */
+		if (prctl(PR_SCHED_CORE, PR_SCHED_CORE_SHARE_FROM, cs->pid, PR_SCHED_CORE_SCOPE_THREAD, NULL) < 0)
+                        err_cookie(cs->pid, cs->cmd);
+		if (prctl(PR_SCHED_CORE, PR_SCHED_CORE_SHARE_TO, cs->dest, cs->scope, NULL) < 0)
+                        err_cookie(cs->pid, cs->cmd);
+
 	}
 
 	/* re-read the cookie */
@@ -182,19 +207,20 @@ static void do_coreset(struct coreset *cs)
 
 int main(int argc, char **argv)
 {
-	pid_t pid = 0;
+	pid_t pid = 0, dest_pid = 0;
 	int c, copy = 0, create = 0, push = 0;
 	int do_exec = 0;
 	struct coreset cs;
 
 	static const struct option longopts[] = {
-		{ "copy",	0, NULL, 'c' },
-		{ "new",	0, NULL, 'n' },
-		{ "pid",	1, NULL, 'p' },
-		{ "scope",	1, NULL, 's' },
-		{ "to",	        0, NULL, 't' },
-		{ "help",	0, NULL, 'h' },
-		{ "version",	0, NULL, 'V' },
+		{ "copy",	no_argument, NULL, 'c' },
+		{ "dest",	required_argument, NULL, 'd' },
+		{ "new",	no_argument, NULL, 'n' },
+		{ "pid",	required_argument, NULL, 'p' },
+		{ "scope",	required_argument, NULL, 's' },
+		{ "to",	        no_argument, NULL, 't' },
+		{ "help",	no_argument, NULL, 'h' },
+		{ "version",	no_argument, NULL, 'V' },
 		{ NULL,		0, NULL,  0  }
 	};
 	static const ul_excl_t excl[] = {       /* rows and cols in ASCII order */
@@ -210,11 +236,14 @@ int main(int argc, char **argv)
 
 	memset(&cs, 0, sizeof(cs));
 
-	while ((c = getopt_long(argc, argv, "+cnp:s:thV", longopts, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "cd:np:s:thV", longopts, NULL)) != -1) {
                 err_exclusive_options(c, longopts, excl, excl_st);
 		switch (c) {
 		case 'c':
 			copy = 1;
+			break;
+		case 'd':
+			dest_pid = strtopid_or_err(optarg, _("invalid copy dest PID argument"));
 			break;
 		case 'n':
 		        create = 1;
@@ -237,14 +266,20 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// pid and no command is okay. No pid and no command is not.  Copy and no command is not okay.
+	// pid and no command is okay. No pid and no command is not.  Copy and no command is okay if dest_pid.
 	// push and no command is okay. copy and push require pid
-	if (((!pid || copy) && argc - optind < 1) || ((copy || push) && !pid)) {
+	if (((!pid || (copy && !dest_pid)) && argc - optind < 1) || ((copy || push) && !pid)) {
 		warnx(_("bad usage"));
 		errtryhelp(EXIT_FAILURE);
 	}
 
-        /* scope must be one of PR_SCHED_CORE_SCOPE_*  */
+	/* ensure we have dest pid only with copy operation */
+	if (dest_pid && !copy) {
+		warnx(_("Dest pid can only be used with copy"));
+		errtryhelp(EXIT_FAILURE);
+	}
+        
+	/* scope must be one of PR_SCHED_CORE_SCOPE_*  */
 	if (cs.scope < PR_SCHED_CORE_SCOPE_THREAD || cs.scope > PR_SCHED_CORE_SCOPE_PROCESS_GROUP) {
 		warnx(_("invalid scope"));
 		errtryhelp(EXIT_FAILURE);
@@ -255,15 +290,19 @@ int main(int argc, char **argv)
 
 	if (create)
 		cs.cmd = CORE_CREATE;
-	else if (copy)
+	else if (dest_pid) {
+		cs.cmd = CORE_COPYPUSH;
+		cs.dest = dest_pid;
+	} else if (copy)
 		cs.cmd = CORE_COPY;
 	else if (push)
 		cs.cmd = CORE_PUSH;
-	else cs.cmd = CORE_SHOW;
+	else
+		cs.cmd = CORE_SHOW;
 
 	/* create and show with a pid don't use the command */
-	if (pid && do_exec && (cs.cmd == CORE_SHOW || cs.cmd == CORE_CREATE)) {
-		warnx(_("Ingoring extraneous input"));
+	if ((pid  || dest_pid) && do_exec && (cs.cmd == CORE_SHOW || cs.cmd == CORE_CREATE)) {
+		warnx(_("Ignoring extraneous input"));
 		do_exec = 0;
 	}
 
