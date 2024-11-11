97bae41f81cb313f8c73bf7e05c497a97829fdfa
Karel Zak <kzak@redhat.com>
2024-11-11 15:05:59 +0100

    findmnt: improve --help output
    The current help output is quite lengthy. It would be beneficial to
    organize it into sections, such as data sources, data filters, and
    options. This would make it easier for users to navigate and
    understand the available features.
    
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- misc-utils/findmnt.c
+++ misc-utils/findmnt.c
@@ -1510,59 +1510,65 @@ static void __attribute__((__noreturn__)) usage(void)
 	fputs(USAGE_SEPARATOR, out);
 	fputs(_("Find a (mounted) filesystem.\n"), out);
 
-	fputs(USAGE_OPTIONS, out);
-	fputs(_(" -s, --fstab            search in static table of filesystems\n"), out);
+
+	fputs(_("\nData sources:\n"), out);
+	fputs(_(" -F, --tab-file <path>  alternative file for -s, -m or -k options\n"), out);
 	fputs(_(" -m, --mtab             search in table of mounted filesystems\n"
 		"                          (includes user space mount options)\n"), out);
 	fputs(_(" -k, --kernel[=<method>] search in kernel mount table (default)\n"
 		"                          <method> is mountinfo or listmount\n"), out);
-	fputc('\n', out);
+	fputs(_(" -N, --task <tid>       use alternative namespace (/proc/<tid>/mountinfo file)\n"), out);
 	fputs(_(" -p, --poll[=<list>]    monitor changes in table of mounted filesystems\n"), out);
-	fputs(_(" -w, --timeout <num>    upper limit in milliseconds that --poll will block\n"), out);
-	fputc('\n', out);
+	fputs(_(" -s, --fstab            search in static table of filesystems\n"), out);
 
+
+
+	fputs(_("\nData filters:\n"), out);
 	fputs(_(" -A, --all              disable all built-in filters, print all filesystems\n"), out);
+	fputs(_(" -d, --direction <word> direction of search, 'forward' or 'backward'\n"), out);
+	fputs(_(" -f, --first-only       print the first found filesystem only\n"), out);
+	fputs(_(" -i, --invert           invert the sense of matching\n"), out);
+	fputs(_("     --pseudo           print only pseudo-filesystems\n"), out);
+	fputs(_(" -Q, --filter <expr>    apply display filter\n"), out);
+	fputs(_(" -M, --mountpoint <dir> the mountpoint directory\n"), out);
+	fputs(_("     --shadowed         print only filesystems over-mounted by another filesystem\n"), out);
+	fputs(_(" -R, --submounts        print all submounts for the matching filesystems\n"), out);
+	fputs(_("     --real             print only real filesystems\n"), out);
+	fputs(_(" -S, --source <string>  the device to mount (by name, maj:min, \n"
+	        "                          LABEL=, UUID=, PARTUUID=, PARTLABEL=)\n"), out);
+	fputs(_(" -T, --target <path>    the path to the filesystem to use\n"), out);
+	fputs(_(" -t, --types <list>     limit the set of filesystems by FS types\n"), out);
+	fputs(_(" -U, --uniq             ignore filesystems with duplicate target\n"), out);
+
+
+	fputs(USAGE_OPTIONS, out);
 	fputs(_(" -a, --ascii            use ASCII chars for tree formatting\n"), out);
 	fputs(_(" -b, --bytes            print sizes in bytes rather than in human readable format\n"), out);
 	fputs(_(" -C, --nocanonicalize   don't canonicalize when comparing paths\n"), out);
 	fputs(_(" -c, --canonicalize     canonicalize printed paths\n"), out);
 	fputs(_(" -D, --df               imitate the output of df(1)\n"), out);
-	fputs(_(" -d, --direction <word> direction of search, 'forward' or 'backward'\n"), out);
 	fputs(_(" -e, --evaluate         convert tags (LABEL,UUID,PARTUUID,PARTLABEL) \n"
 	        "                          to device names\n"), out);
-	fputs(_(" -F, --tab-file <path>  alternative file for -s, -m or -k options\n"), out);
-	fputs(_(" -f, --first-only       print the first found filesystem only\n"), out);
 	fputs(_(" -I, --dfi              imitate the output of df(1) with -i option\n"), out);
-	fputs(_(" -i, --invert           invert the sense of matching\n"), out);
 	fputs(_(" -J, --json             use JSON output format\n"), out);
 	fputs(_(" -l, --list             use list format output\n"), out);
-	fputs(_(" -N, --task <tid>       use alternative namespace (/proc/<tid>/mountinfo file)\n"), out);
 	fputs(_(" -n, --noheadings       don't print column headings\n"), out);
 	fputs(_(" -O, --options <list>   limit the set of filesystems by mount options\n"), out);
 	fputs(_(" -o, --output <list>    output columns (see --list-columns)\n"), out);
 	fputs(_("     --output-all       output all available columns\n"), out);
 	fputs(_(" -P, --pairs            use key=\"value\" output format\n"), out);
-	fputs(_("     --pseudo           print only pseudo-filesystems\n"), out);
-	fputs(_("     --shadowed         print only filesystems over-mounted by another filesystem\n"), out);
-	fputs(_(" -Q, --filter <expr>    apply display filter\n"), out);
-	fputs(_(" -R, --submounts        print all submounts for the matching filesystems\n"), out);
 	fputs(_(" -r, --raw              use raw output format\n"), out);
-	fputs(_("     --real             print only real filesystems\n"), out);
-	fputs(_(" -S, --source <string>  the device to mount (by name, maj:min, \n"
-	        "                          LABEL=, UUID=, PARTUUID=, PARTLABEL=)\n"), out);
-	fputs(_(" -T, --target <path>    the path to the filesystem to use\n"), out);
 	fputs(_("     --tree             enable tree format output if possible\n"), out);
-	fputs(_(" -M, --mountpoint <dir> the mountpoint directory\n"), out);
-	fputs(_(" -t, --types <list>     limit the set of filesystems by FS types\n"), out);
-	fputs(_(" -U, --uniq             ignore filesystems with duplicate target\n"), out);
 	fputs(_(" -u, --notruncate       don't truncate text in columns\n"), out);
 	fputs(_(" -v, --nofsroot         don't print [/dir] for bind or btrfs mounts\n"), out);
+	fputs(_(" -w, --timeout <num>    upper limit in milliseconds that --poll will block\n"), out);
 	fputs(_(" -y, --shell            use column names to be usable as shell variable identifiers\n"), out);
 
 	fputc('\n', out);
 	fputs(_(" -x, --verify           verify mount table content (default is fstab)\n"), out);
 	fputs(_("     --verbose          print more details\n"), out);
 	fputs(_("     --vfs-all          print all VFS options\n"), out);
+
 
 	fputs(USAGE_SEPARATOR, out);
 	fputs(_(" -H, --list-columns     list the available columns\n"), out);
