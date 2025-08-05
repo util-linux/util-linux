223a502b02087bf65b32cf6c896149e0aa1f4404
Benno Schulenberg <bensberg@telfort.nl>
2025-08-05 11:24:41 +0200

    chrt: (man,usage) mark the priority value as optional in the synopses
    The priority value is optional in some cases, so it cannot be listed
    as required.
    
    CC: Madadi Vineeth Reddy <vineethr@linux.ibm.com>
    Signed-off-by: Benno Schulenberg <bensberg@telfort.nl>

--- schedutils/chrt.c
+++ schedutils/chrt.c
@@ -60,8 +60,8 @@ static void __attribute__((__noreturn__)) usage(void)
 	fputs(_("Show or change the real-time scheduling attributes of a process.\n"), out);
 	fputs(USAGE_SEPARATOR, out);
 	fputs(_("Set policy:\n"
-		" chrt [options] <priority> <command> [<argument>...]\n"
-		" chrt [options] --pid <priority> <PID>\n"), out);
+		" chrt [options] [<priority>] <command> [<argument>...]\n"
+		" chrt [options] --pid [<priority>] <PID>\n"), out);
 	fputs(USAGE_SEPARATOR, out);
 	fputs(_("Get policy:\n"
 		" chrt --pid <PID>\n"), out);
