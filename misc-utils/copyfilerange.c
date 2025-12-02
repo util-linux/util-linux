ef03a51a19dea6a3ae4d5d6cb92edac7014e551e
Dick Marinus <dick@mrns.nl>
2025-12-02 20:51:35 +0100

    copyfilerange: new command to call copy-file-range
    This command allows you to call the copy-file range system call which
    performs an in-kernel copy between two files without the additional cost
    of transferring data from the kernel to user space and then back into
    the kernel.
    
    It gives filesystems an opportunity to implement "copy acceleration"
    techniques, such as the use of reflinks (i.e., two or more inodes that
    share  pointers to the same copy-on-write disk blocks) or
    server-side-copy (in the case of NFS).
    
    A purpose of this command is to in-place deduplicate equal file blocks
    in two different files.
    
    When calling this command the source and destination file are required
    and ranges can be supplied using command line arguments or from a
    commands file. The source and destination filename can be supplied using
    the optional --source and --destination options.
    
    A file range contains the offset in the source and destination file and
    the length of the range in bytes. The offsets can be omitted so that the
    current file pointer is used. When the size is omitted or set to zero
    the remainder of the file is copied.
    
    If the file range is too long to fit in SIZE_MAX subsequential calls to
    copy_file_range will be performed to copy the whole range.
    
    The following command will copy/reflink a file:
    
    	$ copyfilerange file1 file2 0::
    
    The following command will copy the second block from file1 into file2:
    
            $ copyfilerange file1 file2 4096:4096:4096
    
    Signed-off-by: Dick Marinus <dick@mrns.nl>

---
+++ misc-utils/copyfilerange.c
@@ -0,0 +1,226 @@
+/*
+ * copyfilerange - utility to use the copy_file_range(2) system call
+ *
+ * Copyright (C) 2025 Dick Marinus <dick@mrns.nl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it would be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <limits.h>
+
+#include "c.h"
+#include "nls.h"
+#include "closestream.h"
+#include "xalloc.h"
+
+static void __attribute__((__noreturn__)) usage(void)
+{
+	FILE *out = stdout;
+	fputs(USAGE_HEADER, out);
+	fprintf(out,
+		_(" %1$s [options] [<source>] [<destination>] [<command>...]\n"),
+		program_invocation_short_name);
+
+	fputs(USAGE_SEPARATOR, out);
+	fputsln(_("Copy file ranges from source to destination file."), out);
+
+	fputs(USAGE_OPTIONS, out);
+	fputsln(_(" --source, -s filename       source filename"), out);
+	fputsln(_(" --destination, -d filename  destination filename"), out);
+	fputsln(_(" --commands, -c filename     read command(s) seperated by newlines from filename"), out);
+	fputsln(_(" source                      source filename"), out);
+	fputsln(_(" destination                 destination filename"), out);
+	fputsln(_(" command                     source_offset:dest_offset:length, all values are in bytes"), out);
+	fputsln(_("                             if length is set to 0 as much as available will be copied"), out);
+	fputsln(_("                             when the offset is omitted the last file position is used"), out);
+
+	fputs(USAGE_SEPARATOR, out);
+	fprintf(out, USAGE_HELP_OPTIONS(16));
+
+	fprintf(out, USAGE_MAN_TAIL("copyfilerange(1)"));
+	exit(EXIT_SUCCESS);
+}
+
+static int parse_range(const char *range_str, off_t *src_off, off_t *dst_off, size_t *len)
+{
+	char *copy = strdup(range_str);
+	if (!copy) return -1;
+
+	char *start;
+
+	start = copy;
+	char *token;
+	token = strchr(start, ':');
+	if (!token) goto fail;
+	*token = 0;
+	if (*start) *src_off = atoll(start);
+
+	start = token + 1;
+	token = strchr(start, ':');
+	if (!token) goto fail;
+	*token = 0;
+	if (*start) *dst_off = atoll(start);
+
+	start = token + 1;
+	if (*start) *len = atoll(start);
+	else *len = 0;
+
+	free(copy);
+	return 0;
+
+fail:
+	free(copy);
+	return -1;
+
+}
+
+static int handle_command(int fd_src, int fd_dst, off_t* src_off, off_t* dst_off, char* command)
+{
+	size_t len;
+	int rc = 0;
+	if (parse_range(command, src_off, dst_off, &len) != 0) {
+		fprintf(stderr, _("invalid range format: %s\n"), command);
+		return 1;
+	}
+
+	if (len == 0) {
+		off_t src_size = lseek(fd_src, 0, SEEK_END);
+		if (src_size == -1)
+			err(EXIT_FAILURE, _("cannot determine size of source file"));
+		len = src_size - *src_off;
+	}
+
+	size_t remaining = len;
+	while (remaining > 0) {
+		size_t chunk = remaining > SIZE_MAX ? SIZE_MAX : remaining;
+		ssize_t copied = copy_file_range(fd_src, src_off, fd_dst, dst_off, chunk, 0);
+		if (copied < 0) {
+			fprintf(stderr, _("failed copy file range %s at source offset %ld: %m\n"), command, *src_off);
+			rc |= 2;
+			break;
+		}
+		if (copied == 0) break;
+		remaining -= copied;
+	}
+	return rc;
+}
+
+int main(int argc, char **argv)
+{
+	char **command_files = NULL;
+	size_t ncommand_files = 0;
+	char *source_filename = NULL;
+	char *destination_filename = NULL;
+	int fd_src, fd_dst;
+	int rc = 0;
+
+	static const struct option longopts[] = {
+		{ "commands",    required_argument, NULL, 'c' },
+		{ "source",      required_argument, NULL, 's' },
+		{ "destination", required_argument, NULL, 'd' },
+		{ "version",     no_argument,       NULL, 'V' },
+		{ "help",        no_argument,       NULL, 'h' },
+		{ NULL, 0, NULL, 0 },
+	};
+
+	setlocale(LC_ALL, "");
+	bindtextdomain(PACKAGE, LOCALEDIR);
+	textdomain(PACKAGE);
+	close_stdout_atexit();
+
+	int c;
+	while ((c = getopt_long (argc, argv, "c:s:d:Vh", longopts, NULL)) != -1) {
+		switch (c) {
+		case 'c':
+			if (!command_files)
+				command_files = xmalloc(sizeof(char *) * argc);
+			command_files[ncommand_files++] = xstrdup(optarg);
+			break;
+		case 's':
+			if (source_filename)
+				errx(EXIT_FAILURE, _("only one source file is allowed (%s already supplied)"), source_filename);
+			source_filename = xstrdup(optarg);
+			break;
+		case 'd':
+			if (destination_filename)
+				errx(EXIT_FAILURE, _("only one destination file is allowed (%s already supplied)"), destination_filename);
+			destination_filename = xstrdup(optarg);
+			break;
+		case 'V':
+			print_version(EXIT_SUCCESS);
+		case 'h':
+			usage();
+		}
+	}
+
+	int rem_optind;
+	for (rem_optind = optind; rem_optind < argc; rem_optind++) {
+		if (source_filename && destination_filename) break;
+		if (!source_filename)
+			source_filename = xstrdup(argv[rem_optind]);
+		else if (!destination_filename)
+			destination_filename = xstrdup(argv[rem_optind]);
+	}
+
+	if (!source_filename)
+		errx(EXIT_FAILURE, _("source file is required"));
+
+	if (!destination_filename)
+		errx(EXIT_FAILURE, _("destination file is required"));
+
+	if ((fd_src = open(source_filename, O_RDONLY)) < 0)
+		err(EXIT_FAILURE, _("cannot open source %s"), argv[1]);
+	free(source_filename);
+
+	if ((fd_dst = open(destination_filename, O_WRONLY | O_CREAT, 0666)) < 0)
+		err(EXIT_FAILURE, _("cannot open destination %s"), argv[2]);
+	free(destination_filename);
+
+	off_t src_off, dst_off;
+
+	for (size_t i = 0; i < ncommand_files; i++) {
+		FILE *f = NULL;
+
+		if (!(f = fopen(command_files[i], "r")))
+			err(EXIT_FAILURE, _("cannot open command file %s"), command_files[i]);
+		free(command_files[i]);
+
+		char *line = NULL;
+		size_t len = 0;
+
+		while (getline(&line, &len, f) != -1) {
+			rc |= handle_command(fd_src, fd_dst, &src_off, &dst_off, line);
+		}
+
+		free(line);
+		fclose(f);
+	}
+	free(command_files);
+
+	for (; rem_optind < argc; rem_optind++) {
+		rc |= handle_command(fd_src, fd_dst, &src_off, &dst_off, argv[rem_optind]);
+	}
+
+	close(fd_src);
+	close(fd_dst);
+	return rc;
+}
