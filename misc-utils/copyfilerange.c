cdc647b51352fca1cef2fe4265f2f2c0cd1ed0c4
Dick Marinus <dick@mrns.nl>
2026-01-15 14:17:31 +0100

    Add a test to make sure the offset from the previous range is preserved

--- misc-utils/copyfilerange.c
+++ misc-utils/copyfilerange.c
@@ -83,7 +83,7 @@ static void __attribute__((__noreturn__)) usage(void)
 
 static int parse_range(const char *str, struct rangeitem *range)
 {
-	char *copy = NULL,  *start = NULL, *token = NULL, rc = 0;
+	char *copy = NULL, *start = NULL, *token = NULL, rc = 0;
 	uintmax_t tmp;
 
 	copy = xstrdup(str);
