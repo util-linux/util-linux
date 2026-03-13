4b74757b7b571dd487ef88ea04010d51a5473e58
Tobias Stoeckmann <tobias@stoeckmann.org>
2026-03-13 18:25:25 +0100

    lib/debug: Move debug functions into c file
    Having the debug functions as static inline functions throughout the
    code base leads to code bloat, since they always serve the same purpose
    without custom modifications.
    
    Keep defines and everything specific to an including file in a header, but
    move function bodies into their own c file.
    
    Reduces size of util-linux installation by up to 80 KB on x86_64.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- include/debug.h
+++ include/debug.h
@@ -34,11 +34,6 @@
  *
  */
 
-#include <stdarg.h>
-#include <string.h>
-
-#include "c.h"
-
 struct ul_debug_maskname {
 	const char *name;
 	int mask;
@@ -106,75 +101,11 @@ struct ul_debug_maskname {
 		__UL_INIT_DEBUG_FROM_STRING(lib, pref, mask, envstr); \
 	} while (0)
 
-
-
-static inline void __attribute__ ((__format__ (__printf__, 1, 2)))
-ul_debug(const char *mesg, ...)
-{
-	va_list ap;
-	va_start(ap, mesg);
-	vfprintf(stderr, mesg, ap);
-	va_end(ap);
-	fputc('\n', stderr);
-}
-
-static inline int ul_debug_parse_mask(
-			const struct ul_debug_maskname flagnames[],
-			const char *mask)
-{
-	int res;
-	char *ptr;
-
-	/* let's check for a numeric mask first */
-	res = strtoul(mask, &ptr, 0);
-
-	/* perhaps it's a comma-separated string? */
-	if (ptr && *ptr && flagnames && flagnames[0].name) {
-		char *msbuf, *ms, *name;
-		res = 0;
-
-		ms = msbuf = strdup(mask);
-		if (!ms)
-			return res;
-
-		while ((name = strtok_r(ms, ",", &ptr))) {
-			const struct ul_debug_maskname *d;
-			ms = ptr;
-
-			for (d = flagnames; d && d->name; d++) {
-				if (strcmp(name, d->name) == 0) {
-					res |= d->mask;
-					break;
-				}
-			}
-			/* nothing else we can do by OR-ing the mask */
-			if (res == 0xffff)
-				break;
-		}
-		free(msbuf);
-	} else if (ptr && strcmp(ptr, "all") == 0)
-		res = 0xffff;
-
-	return res;
-}
-
-static inline void ul_debug_print_masks(
-			const char *env,
-			const struct ul_debug_maskname flagnames[])
-{
-	const struct ul_debug_maskname *d;
-
-	if (!flagnames)
-		return;
-
-	fprintf(stderr, "Available \"%s=<name>[,...]|<mask>\" debug masks:\n",
-			env);
-	for (d = flagnames; d && d->name; d++) {
-		if (!d->help)
-			continue;
-		fprintf(stderr, "   %-8s [0x%06x] : %s\n",
-				d->name, d->mask, d->help);
-	}
-}
+extern void ul_debug(const char *mesg, ...)
+		__attribute__ ((__format__ (__printf__, 1, 2)));
+extern int ul_debug_parse_mask(const struct ul_debug_maskname flagnames[],
+			       const char *mask);
+extern void ul_debug_print_masks(const char *env,
+				 const struct ul_debug_maskname flagnames[]);
 
 #endif /* UTIL_LINUX_DEBUG_H */
