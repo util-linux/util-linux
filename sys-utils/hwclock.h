6252cea38f66cb1ccc07916ddf520e86cd429778
Stefan Kerkmann <s.kerkmann@pengutronix.de>
2026-07-30 12:13:16 +0200

    hwclock-rtc: add RTC_PARAM_BATTERY_LOW_DETECT support
    Some rtcs like the pcf2131 do not enable low battery detection by
    default. A new linux patch series[1] exposes a parameter to enable the
    detection, support is implemented in this commit.
    
    [1]: https://lore.kernel.org/all/20260311200237.3531981-1-hugo@hugovil.com/
    
    Signed-off-by: Stefan Kerkmann <s.kerkmann@pengutronix.de>

--- sys-utils/hwclock.h
+++ sys-utils/hwclock.h
@@ -42,6 +42,7 @@ struct rtc_param {
 #  define RTC_PARAM_FEATURES		0
 #  define RTC_PARAM_CORRECTION		1
 #  define RTC_PARAM_BACKUP_SWITCH_MODE	2
+#  define RTC_PARAM_BATTERY_LOW_DETECT	3
 # endif /* RTC_PARAM_GET */
 
 # ifndef RTC_VL_READ
