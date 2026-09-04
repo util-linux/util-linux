c6890e43c886dc45a009f0e63e28ad333d2adb03
Werner Fink <werner@suse.de>
2026-09-04 14:09:14 +0200

    Convert indentation from 4+4 to normal tab stops
    Signed-off-by: Werner Fink <werner@suse.de>

--- login-utils/vmcp.c
+++ login-utils/vmcp.c
@@ -25,27 +25,27 @@
 
 int isinteger(const char *str)
 {
-    int errs = errno, ret = 1;
-    long __attribute__((__unused__)) val;
-    char *endptr;
+	int errs = errno, ret = 1;
+	long __attribute__ ((__unused__)) val;
+	char *endptr;
 
-    errno = 0;
-    val = strtol(str, &endptr, 10);
+	errno = 0;
+	val = strtol(str, &endptr, 10);
 
-    /* Check if no digits were found at all */
-    if (str == endptr)
-	ret = 0;
+	/* Check if no digits were found at all */
+	if (str == endptr)
+		ret = 0;
 
-    /* Check for overflow or underflow */
-    if (errno == ERANGE)
-	ret = 0;
+	/* Check for overflow or underflow */
+	if (errno == ERANGE)
+		ret = 0;
 
-    /* Check for trailing non-numeric characters (e.g., "123abc") */
-    if (*endptr != '\0')
-	ret = 0;
+	/* Check for trailing non-numeric characters (e.g., "123abc") */
+	if (*endptr != '\0')
+		ret = 0;
 
-    errno = errs;
-    return ret;
+	errno = errs;
+	return ret;
 }
 
 #if defined(__s390__) || defined(__s390x__)
@@ -60,172 +60,173 @@ static int spooling;
 
 int openvmcp(void)
 {
-    return open(VMCP_DEVICE_NODE, O_RDWR|O_NOCTTY);
+	return open(VMCP_DEVICE_NODE, O_RDWR | O_NOCTTY);
 }
 
 void clearvmcp(void)
 {
-    if (more) {
-	free(more);
-	more = NULL;
-    }
-    if (hold) {
-	free(hold);
-	hold = NULL;
-    }
+	if (more) {
+		free(more);
+		more = NULL;
+	}
+	if (hold) {
+		free(hold);
+		hold = NULL;
+	}
 }
 
-static char* askvmcp(int fd, const char *question)
+static char *askvmcp(int fd, const char *question)
 {
-    long pagesize = sysconf(_SC_PAGESIZE);
-    int rc = 0, num, buffersize;
-    char* ret = NULL;
+	long pagesize = sysconf(_SC_PAGESIZE);
+	int rc = 0, num, buffersize;
+	char *ret = NULL;
 
-    num = (strlen(question) + pagesize - 1)/pagesize;
-    buffersize = num * pagesize;
+	num = (strlen(question) + pagesize - 1) / pagesize;
+	buffersize = num * pagesize;
 
-    if (ioctl(fd, VMCP_SETBUF, &buffersize) == -1)
-	goto out;
-    if (ul_write_all(fd, question, strlen(question)) != 0)
-        goto out;
-    if (ioctl(fd, VMCP_GETCODE, &rc) == -1)
-	goto out;
-    if (ioctl(fd, VMCP_GETSIZE, &buffersize) == -1)
-	goto out;
-    ret = (char*)malloc(buffersize);
-    if (!ret)
-	goto out;
-    if (ul_read_all(fd, ret, buffersize) != 0) {
-	free(ret);
-	ret = NULL;
-    }
-out:
-    return ret;
+	if (ioctl(fd, VMCP_SETBUF, &buffersize) == -1)
+		goto out;
+	if (ul_write_all(fd, question, strlen(question)) != 0)
+		goto out;
+	if (ioctl(fd, VMCP_GETCODE, &rc) == -1)
+		goto out;
+	if (ioctl(fd, VMCP_GETSIZE, &buffersize) == -1)
+		goto out;
+	ret = (char *)malloc(buffersize);
+	if (!ret)
+		goto out;
+	if (ul_read_all(fd, ret, buffersize) != 0) {
+		free(ret);
+		ret = NULL;
+	}
+ out:
+	return ret;
 }
 
-char* queryterm(int fd)
+char *queryterm(int fd)
 {
-    const char* question = "QUERY TERMINAL";
-    /* Reset cached terminal state before reparsing a fresh QUERY TERMINAL reply. */
-    clearvmcp();
-    return askvmcp(fd, question);
+	const char *question = "QUERY TERMINAL";
+	/* Reset cached terminal state before reparsing a fresh QUERY TERMINAL reply. */
+	clearvmcp();
+	return askvmcp(fd, question);
 }
 
-char* queryspool(int fd)
+char *queryspool(int fd)
 {
-    const char* question = "QUERY VIRTUAL CONSOLE";
-    return askvmcp(fd, question);
+	const char *question = "QUERY VIRTUAL CONSOLE";
+	return askvmcp(fd, question);
 }
 
 static int writevmcp(int fd, char *instruction)
 {
-    long pagesize = sysconf(_SC_PAGESIZE);
-    int rc = 0, num, buffersize;
-    int ret = -1;
+	long pagesize = sysconf(_SC_PAGESIZE);
+	int rc = 0, num, buffersize;
+	int ret = -1;
 
-    num = (strlen(instruction) + pagesize - 1)/pagesize;
-    buffersize = num * pagesize;
+	num = (strlen(instruction) + pagesize - 1) / pagesize;
+	buffersize = num * pagesize;
 
-    if (ioctl(fd, VMCP_SETBUF, &buffersize) == -1)
-	goto out;
-    do {
-	rc = write(fd, instruction, strlen(instruction));
-	if (rc < 0) {
-	    if (errno != EINTR)
+	if (ioctl(fd, VMCP_SETBUF, &buffersize) == -1)
 		goto out;
-	}
-    } while (rc < 0);
-    if (ioctl(fd, VMCP_GETCODE, &rc) == -1)
-	goto out;
-    if (ioctl(fd, VMCP_GETSIZE, &buffersize) == -1)
-	goto out;
-    if (rc == 0 && buffersize == 0)
-	ret = 0;
-out:
-    return ret;
+	do {
+		rc = write(fd, instruction, strlen(instruction));
+		if (rc < 0) {
+			if (errno != EINTR)
+				goto out;
+		}
+	} while (rc < 0);
+	if (ioctl(fd, VMCP_GETCODE, &rc) == -1)
+		goto out;
+	if (ioctl(fd, VMCP_GETSIZE, &buffersize) == -1)
+		goto out;
+	if (rc == 0 && buffersize == 0)
+		ret = 0;
+ out:
+	return ret;
 }
 
 int setterm(int fd, char *tout)
 {
-    char *instruction;
-    int ret = -1;
+	char *instruction;
+	int ret = -1;
 
-    if (asprintf(&instruction, "TERMINAL MORE %s 0 HOLD OFF", tout) == -1)
-	goto out;
+	if (asprintf(&instruction, "TERMINAL MORE %s 0 HOLD OFF", tout) == -1)
+		goto out;
 
-    ret = writevmcp(fd, instruction);
-    free(instruction);
-out:
-    return ret;
+	ret = writevmcp(fd, instruction);
+	free(instruction);
+ out:
+	return ret;
 }
 
 int restoreterm(int fd)
 {
-    char* instruction;
-    int ret = -1;
+	char *instruction;
+	int ret = -1;
 
-    if (!more || !hold)
-	goto out;
-    if (asprintf(&instruction, "TERMINAL %s %s", more, hold) == -1)
-	goto out;
+	if (!more || !hold)
+		goto out;
+	if (asprintf(&instruction, "TERMINAL %s %s", more, hold) == -1)
+		goto out;
 
-    ret = writevmcp(fd, instruction);
-    free(instruction);
-out:
-    return ret;
+	ret = writevmcp(fd, instruction);
+	free(instruction);
+ out:
+	return ret;
 }
 
 int stopspool(int fd)
 {
-    char* instruction = "SPOOL CONSOLE STOP";
-    if (!spooling)
-	return 1;
-    return writevmcp(fd, instruction);
+	char *instruction = "SPOOL CONSOLE STOP";
+	if (!spooling)
+		return 1;
+	return writevmcp(fd, instruction);
 }
 
 int restorespool(int fd)
 {
-    char* instruction = "SPOOL CONSOLE START";
-    if (!spooling)
-	return 1;
-    return writevmcp(fd, instruction);
+	char *instruction = "SPOOL CONSOLE START";
+	if (!spooling)
+		return 1;
+	return writevmcp(fd, instruction);
 }
 
 void parseterm(char *msg)
 {
-    char *token;
+	char *token;
 
-    for (token = strtok(msg, ",\n"); token != NULL; token = strtok(NULL, ",\n")) {
-	if (hold && more)
-	    break;
+	for (token = strtok(msg, ",\n"); token != NULL;
+	     token = strtok(NULL, ",\n")) {
+		if (hold && more)
+			break;
 
-	while (*token == ' ')
-	    token++;
+		while (*token == ' ')
+			token++;
 
-	if (strncmp("MORE ", token, 5) == 0)
-	    more = strdup(token);
-	if (strncmp("HOLD ", token, 5) == 0)
-	    hold = strdup(token);
-    }
+		if (strncmp("MORE ", token, 5) == 0)
+			more = strdup(token);
+		if (strncmp("HOLD ", token, 5) == 0)
+			hold = strdup(token);
+	}
 }
 
 void parsespool(char *msg)
 {
-    spooling = 0;
-    if (strstr(msg, " TERM START ") != NULL)
-	spooling = 1;
+	spooling = 0;
+	if (strstr(msg, " TERM START ") != NULL)
+		spooling = 1;
 }
 
 void warning3215(int fd)
 {
-    /*
-     * Warning: Do not translate this test as it might inlude then so called
-     * umlauts which in fact can not be encoded for the 3215 console interface.
-     * The 3215 console driver work in fact with EBCDIC codepage and the
-     * kernel has to translate such umlauts (multi or single bytes) with the
-     * correct EBCDIC character table (for german e.g. IBM-1141 or IBM-273).
-     */
-    (void) writevmcp(fd, "MESSAGE * WARNING: 3215 mode. Password visible!");
-    (void) writevmcp(fd, "MESSAGE * Ensure nobody is watching the screen.");
+	/*
+	 * Warning: Do not translate this test as it might inlude then so called
+	 * umlauts which in fact can not be encoded for the 3215 console interface.
+	 * The 3215 console driver work in fact with EBCDIC codepage and the
+	 * kernel has to translate such umlauts (multi or single bytes) with the
+	 * correct EBCDIC character table (for german e.g. IBM-1141 or IBM-273).
+	 */
+	(void)writevmcp(fd, "MESSAGE * WARNING: 3215 mode. Password visible!");
+	(void)writevmcp(fd, "MESSAGE * Ensure nobody is watching the screen.");
 }
 #endif
